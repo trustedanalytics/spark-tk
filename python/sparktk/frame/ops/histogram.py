72baf1fbc856a2f29e683bb601629e2031213799
dmsuehir <dina.m.suehiro@intel.com>
2016-04-08 15:14:41 -0700

    Updating py code for creating histogram object.

--- python/sparktk/frame/ops/histogram.py
+++ python/sparktk/frame/ops/histogram.py
@@ -133,9 +133,9 @@ def histogram(self, column_name, num_bins=None, weight_column_name=None, bin_typ
                 A list containing a decimal containing the percentage of
                 observations found in the total set per bin.
     """
-    dict = self._tc.jutils.convert.scala_map_string_seq_to_python(self._scala.histogram(column_name,
+    results = self._tc.jutils.convert.scala_map_string_seq_to_python(self._scala.histogram(column_name,
                           self._tc.jutils.convert.to_scala_option(num_bins),
                           self._tc.jutils.convert.to_scala_option(weight_column_name),
                           bin_type))
-    return Histogram(dict["cutoffs"], dict["hist"], dict["density"])
+    return Histogram(**results)
 
