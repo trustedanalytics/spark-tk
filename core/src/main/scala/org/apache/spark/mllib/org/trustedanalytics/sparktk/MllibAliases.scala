ad302a30e37c3195365a14fe4cc72075dfb19d32
Briton Barker <briton.barker@intel.com>
2016-04-22 12:15:43 -0700

    cleaned up spark mllib aliases, added integration-tests README

--- core/src/main/scala/org/apache/spark/mllib/org/trustedanalytics/sparktk/MllibAliases.scala
+++ core/src/main/scala/org/apache/spark/mllib/org/trustedanalytics/sparktk/MllibAliases.scala
@@ -1,10 +1,13 @@
 package org.apache.spark.mllib.org.trustedanalytics.sparktk
 
-import org.apache.spark.mllib.util.{ Loader => SparkLoader }
+import org.apache.spark.mllib.util.{ Loader => SparkLoader, NumericParser => SparkNumericParser }
+import org.apache.spark.mllib.linalg.{ Vectors => SparkVectors, Vector => SparkVector }
 
 object MllibAliases {
 
   val Loader = SparkLoader
+  val NumericParser = SparkNumericParser
+  def parseNumeric(any: Any): SparkVector = SparkVectors.parseNumeric(any)
 
 }
 
