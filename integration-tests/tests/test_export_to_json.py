c4e843e64c5198cfbee0e93e98fdbf881fff06ca
Karthik Vadla <karthik.vadla@intel.com>
2016-05-17 14:19:51 -0700

    Added integration test for export_to_json

--- integration-tests/tests/test_export_to_json.py
+++ integration-tests/tests/test_export_to_json.py
@@ -1,6 +1,9 @@
 from setup import tc, rm, get_sandbox_path
 from sparktk.dtypes import float32
 import os.path
+import shutil
+import subprocess
+import json
 
 def test_export_to_json_file_path(tc):
     print "create frame"
@@ -16,5 +19,32 @@ def test_export_to_json_file_path(tc):
     assert(frame.column_names[4], "dot_product", "frame must have last column name as dot_product")
 
     print "export dp frame in json format to local file system"
+    if(os.path.exists("jsonfile12") == True):
+        shutil.rmtree("jsonfile12")
     frame.export_to_json("jsonfile12")
     assert(os.path.exists("jsonfile12") == False, "export_to_json should export frame in json format to jsonfile12 folder")
+    print "Removing created file"
+    shutil.rmtree("jsonfile12")
+
+
+
+def test_strange_strings(tc):
+    print "create frame"
+    rows = [["A", "Hi, i'm here"],["B", "Hello's to everyone"],["C", "I'm good ~how are doing@"],["DD", "#$this is something, amazing''s"]]
+    schema = [("name", str), ("message", str)]
+    frame = tc.to_frame(rows, schema)
+
+    assert(frame.row_count, 4, "frame should have 4 rows")
+    assert(frame.column_names, ['name', 'message'])
+
+    print "export frame in json format to local file system"
+    if(os.path.exists("jsonstrangestring") == True):
+        shutil.rmtree("jsonstrangestring")
+    frame.export_to_json("jsonstrangestring")
+    assert(os.path.exists("jsonstrangestring") == False, "export_to_json should export frame in json format to jsonstrangestring folder")
+
+    data = subprocess.Popen("cat jsonstrangestring/* | grep 'DD'", stdout=subprocess.PIPE, shell=True).communicate()[0]
+    json_data = json.loads(data)
+    assert(json_data['message'] == frame.take(4).data[3][1], "the value for DD should be #$this is something, amazing''s")
+    print "Removing created file"
+    shutil.rmtree("jsonstrangestring")
