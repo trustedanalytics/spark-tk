b5629fb45aab6ffed4caff42567901cec326096f
Briton Barker <briton.barker@intel.com>
2016-05-23 17:02:39 -0700

    Added README to doc

--- python/sparktk/doc/builddoc.sh
+++ python/sparktk/doc/builddoc.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# buids the python documentation using pdoc
+# builds the python documentation using pdoc
+
 
 NAME="[`basename $0`]"
 DIR="$( cd "$( dirname "$0" )" && pwd )"
@@ -16,9 +17,8 @@ TMP_SPARKTK_DIR=$tmp_dir/sparktk
 
 # skip documenting the doc
 rm -r $TMP_SPARKTK_DIR/doc
-#rm -r $TMP_SPARKTK_DIR/tests
 
-echo $NAME pre-processing the python
+echo $NAME pre-processing the python for the special doctest flags
 python2.7 -m doctools -py=$TMP_SPARKTK_DIR
 
 
@@ -27,14 +27,17 @@ pushd $TMP_SPARKTK_DIR > /dev/null
 
 TMP_SPARKTK_PARENT_DIR="$(dirname "$TMP_SPARKTK_DIR")"
 TEMPLATE_DIR=$SPARKTK_DIR/doc/templates
+
+# specify output folder:
 HTML_DIR=$SPARKTK_DIR/doc/html
 
+# call pdoc
 echo $NAME PYTHONPATH=$TMP_SPARKTK_PARENT_DIR pdoc --only-pypath --html --html-dir=$HTML_DIR --template-dir $TEMPLATE_DIR --overwrite sparktk
 PYTHONPATH=$TMP_SPARKTK_PARENT_DIR pdoc --only-pypath --html --html-dir=$HTML_DIR --template-dir $TEMPLATE_DIR --overwrite sparktk
 
 popd > /dev/null
 
-# Post-processing:  1. Patch "Up" links   2. Process the special doctest flags
+# Post-processing:  Patch the "Up" links
 echo $NAME post-processing the HTML
 python2.7 -m doctools -html=$HTML_DIR
 
