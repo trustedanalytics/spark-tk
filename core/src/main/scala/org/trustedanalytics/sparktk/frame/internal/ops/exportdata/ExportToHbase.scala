e0cf8e712fc1ce1bc011168881914d9c64556e22
Karthik Vadla <karthik.vadla@intel.com>
2016-05-25 10:31:27 -0700

    hbase code base added

--- core/src/main/scala/org/trustedanalytics/sparktk/frame/internal/ops/exportdata/ExportToHbase.scala
+++ core/src/main/scala/org/trustedanalytics/sparktk/frame/internal/ops/exportdata/ExportToHbase.scala
@@ -1,5 +1,15 @@
 package org.trustedanalytics.sparktk.frame.internal.ops.exportdata
 
+import com.sun.tools.doclets.internal.toolkit.Configuration
+import org.aopalliance.intercept.Invocation
+import org.apache.commons.lang3.StringUtils
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.mapreduce.{ Job }
+import org.apache.hadoop.hbase.mapred.TableOutputFormat
+import org.apache.hadoop.hbase.{HColumnDescriptor, HTableDescriptor, HBaseConfiguration}
+import org.apache.hadoop.hbase.client.{Put, HBaseAdmin}
+import org.trustedanalytics.sparktk.frame.{Schema, DataTypes}
 import org.trustedanalytics.sparktk.frame.internal.rdd.FrameRdd
 import org.trustedanalytics.sparktk.frame.internal.{ FrameState, FrameSummarization, BaseFrame }
 
@@ -33,5 +43,87 @@ object ExportToHbase {
                          tableName: String,
                          keyColumnName: Option[String],
                          familyName: String) = {
+    
+    val conf = createConfig(tableName)
+    val familyName = familyName
+
+    val pairRdd = convertToPairRDD(frameRdd,
+      familyName,
+      keyColumnName.getOrElse(StringUtils.EMPTY))
+
+    val hBaseAdmin = new HBaseAdmin(HBaseConfiguration.create())
+    if (!hBaseAdmin.tableExists(tableName)) {
+      val desc = new HTableDescriptor(tableName)
+      desc.addFamily(new HColumnDescriptor(familyName))
+
+      hBaseAdmin.createTable(desc)
+    }
+
+    pairRdd.saveAsNewAPIHadoopDataset(conf)
+  }
+
+  /**
+    * Creates pair rdd to save to hbase
+    *
+    * @param rdd initial frame rdd
+    * @param familyColumnName family column name for hbase
+    * @param keyColumnName key column name for hbase
+    * @param invocation information about the user and the circumstances at the time of the call, as well as a function
+    *                   that can be called to produce a SparkContext that can be used during this invocation
+    * @return pair rdd
+    */
+  def convertToPairRDD(rdd: FrameRdd,
+                       familyColumnName: String,
+                       keyColumnName: String)(implicit invocation: Invocation) = {
+
+    rdd.mapRows(_.valuesAsArray()).zipWithUniqueId().map {
+      case (row, index) => buildRow((row, index), rdd.frameSchema, familyColumnName, keyColumnName)
+    }
+  }
+
+  /**
+    * Create initial configuration for hbase writer
+    *
+    * @param tableName name of hBase table
+    * @return hBase configuration
+    */
+  private def createConfig(tableName: String): Configuration = {
+    val conf = HBaseConfiguration.create()
+    conf.set(TableOutputFormat.OUTPUT_TABLE, tableName)
+    val job = new Job(conf)
+    job.setOutputFormatClass(classOf[TableOutputFormat[ImmutableBytesWritable]])
+
+    job.getConfiguration
+  }
+
+  /**
+    * Builds a row
+    *
+    * @param row row of the original frame
+    * @param schema original schema
+    * @param familyColumnName family column name for hbase
+    * @param keyColumnName key column name for hbase
+    * @return hbase row
+    */
+  private def buildRow(row: (Array[Any], Long), schema: Schema, familyColumnName: String, keyColumnName: String) = {
+    val columnTypes = schema.columns.map(_.dataType)
+    val columnNames = schema.columns.map(_.name)
+    val familyColumnAsByteArray = Bytes.toBytes(familyColumnName)
+
+    val valuesAsDataTypes = DataTypes.parseMany(columnTypes.toArray)(row._1)
+    val valuesAsByteArray = valuesAsDataTypes.map(value => {
+      if (null == value) null else Bytes.toBytes(value.toString)
+    })
+
+    val keyColumnValue = Bytes.toBytes(keyColumnName + row._2)
+    val put = new Put(keyColumnValue)
+    for (index <- 0 to valuesAsByteArray.length - 1) {
+      if (valuesAsByteArray(index) != null) {
+        put.add(familyColumnAsByteArray, Bytes.toBytes(columnNames(index)), valuesAsByteArray(index))
+      }
+    }
+
+    (new ImmutableBytesWritable(keyColumnValue), put)
   }
 }
+
