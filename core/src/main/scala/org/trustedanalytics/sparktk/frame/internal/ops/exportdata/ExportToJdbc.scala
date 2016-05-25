70c3f042550adbdd2b17231543d45cd6dbd85cfc
Karthik Vadla <karthik.vadla@intel.com>
2016-05-25 11:41:50 -0700

    added back dependencies and added usgae explantion in python api

--- core/src/main/scala/org/trustedanalytics/sparktk/frame/internal/ops/exportdata/ExportToJdbc.scala
+++ core/src/main/scala/org/trustedanalytics/sparktk/frame/internal/ops/exportdata/ExportToJdbc.scala
@@ -4,18 +4,18 @@ import java.sql.SQLException
 import java.util.Properties
 import org.apache.commons.lang.StringUtils
 import org.trustedanalytics.sparktk.frame.internal.rdd.FrameRdd
-import org.trustedanalytics.sparktk.frame.internal.{FrameState, FrameSummarization, BaseFrame}
+import org.trustedanalytics.sparktk.frame.internal.{ FrameState, FrameSummarization, BaseFrame }
 
 trait ExportToJdbcSummarization extends BaseFrame {
 
   /**
-    * Write current frame to JDBC table.
-    *
-    * Table will be created or appended to.Export of Vectors is not currently supported.
-    *
-    * @param connectionUrl JDBC connection url to database server
-    * @param tableName     JDBC table name
-    */
+   * Write current frame to JDBC table.
+   *
+   * Table will be created or appended to.Export of Vectors is not currently supported.
+   *
+   * @param connectionUrl JDBC connection url to database server
+   * @param tableName     JDBC table name
+   */
   def exportToJdbc(connectionUrl: String, tableName: String) = {
     execute(ExportToJdbc(connectionUrl, tableName))
   }
--- python/sparktk/frame/ops/export_data.py
+++ python/sparktk/frame/ops/export_data.py
@@ -7,5 +7,36 @@ def export_to_jdbc(self, connection_url, table_name):
     :param connection_url: JDBC connection url to database server
     :param table_name: JDBC table name
 
+    Example
+    --------
+        code snippet:
+
+        - from sparktk import TkContext
+        - tc=TkContext(sc)
+        - data = [[1, 0.2, -2, 5], [2, 0.4, -1, 6], [3, 0.6, 0, 7], [4, 0.8, 1, 8]]
+        - schema = [('a', int), ('b', float),('c', int) ,('d', int)]
+        - my_frame = tc.to_frame(data, schema)
+
+
+        connection_url : (string) : "jdbc:{datasbase_type}://{host}/{database_name}
+
+        Sample connection string for postgres
+        ex: jdbc:postgresql://localhost/postgres [standard connection string to connect to default 'postgres' database]
+
+        table_name: (string): table name. It will create new table with given name if it does not exists already.
+
+        - my_frame.export_to_jdbc("jdbc:postgresql://localhost/postgres", "demo_test")
+
+        Verify exported frame in postgres
+
+        From bash shell
+
+        $sudo -su ppostgres psql
+        postgres=#\d
+
+        You should see demo_test table.
+
+        Run postgres=#select * from demo_test (to verify frame).
+
     """
     self._scala.exportToJdbc(connection_url, table_name)
