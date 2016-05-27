b058ad9112ee989f81c8d7e1a57d09711e864e8f
Karthik Vadla <karthik.vadla@intel.com>
2016-05-27 10:41:33 -0700

    Dpng 7181- Export-to-Json (#11)
    * imported export_to_json
    
    * ExportToJson checkpoing
    
    * Added integration test for export_to_json
    
    * Feedback changes
    
    * rename strArray to jsonAst

--- core/src/main/scala/org/trustedanalytics/sparktk/frame/Frame.scala
+++ core/src/main/scala/org/trustedanalytics/sparktk/frame/Frame.scala
@@ -11,7 +11,7 @@ import org.trustedanalytics.sparktk.frame.internal.ops.binning.{ BinColumnTransf
 import org.trustedanalytics.sparktk.frame.internal.ops.classificationmetrics.{ MultiClassClassificationMetricsSummarization, BinaryClassificationMetricsSummarization }
 import org.trustedanalytics.sparktk.frame.internal.ops.cumulativedist.{ CumulativePercentTransform, CumulativeSumTransform, EcdfSummarization, TallyPercentTransform, TallyTransform }
 import org.trustedanalytics.sparktk.frame.internal.ops.sample.AssignSampleTransform
-import org.trustedanalytics.sparktk.frame.internal.ops.exportdata.ExportToCsvSummarization
+import org.trustedanalytics.sparktk.frame.internal.ops.exportdata.{ ExportToCsvSummarization, ExportToJsonSummarization }
 import org.trustedanalytics.sparktk.frame.internal.ops.flatten.FlattenColumnsTransform
 import org.trustedanalytics.sparktk.frame.internal.ops.RenameColumnsTransform
 import org.trustedanalytics.sparktk.frame.internal.ops.sortedk.SortedKSummarization
@@ -48,6 +48,7 @@ class Frame(frameRdd: RDD[Row], frameSchema: Schema) extends BaseFrame // params
     with EcdfSummarization
     with EntropySummarization
     with ExportToCsvSummarization
+    with ExportToJsonSummarization
     with FlattenColumnsTransform
     with HistogramSummarization
     with MultiClassClassificationMetricsSummarization
