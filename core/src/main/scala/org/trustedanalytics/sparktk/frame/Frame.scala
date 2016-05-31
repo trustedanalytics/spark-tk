91276467356ee30a7205e706b1437c557518fdcf
Karthik Vadla <karthik.vadla@intel.com>
2016-05-31 11:12:18 -0700

    Dpng 7289 hbase (#15)
    * added export to hbase file in scala
    
    * hbase code base added
    
    * added hbase code
    
    * changes to doctest
    
    * issue fixed, when different family name is given for the table alreay exists

--- core/src/main/scala/org/trustedanalytics/sparktk/frame/Frame.scala
+++ core/src/main/scala/org/trustedanalytics/sparktk/frame/Frame.scala
@@ -11,7 +11,7 @@ import org.trustedanalytics.sparktk.frame.internal.ops.binning.{ BinColumnTransf
 import org.trustedanalytics.sparktk.frame.internal.ops.classificationmetrics.{ MultiClassClassificationMetricsSummarization, BinaryClassificationMetricsSummarization }
 import org.trustedanalytics.sparktk.frame.internal.ops.cumulativedist.{ CumulativePercentTransform, CumulativeSumTransform, EcdfSummarization, TallyPercentTransform, TallyTransform }
 import org.trustedanalytics.sparktk.frame.internal.ops.sample.AssignSampleTransform
-import org.trustedanalytics.sparktk.frame.internal.ops.exportdata.{ ExportToCsvSummarization, ExportToHiveSummarization, ExportToJdbcSummarization, ExportToJsonSummarization }
+import org.trustedanalytics.sparktk.frame.internal.ops.exportdata.{ ExportToCsvSummarization, ExportToHbaseSummarization, ExportToHiveSummarization, ExportToJdbcSummarization, ExportToJsonSummarization }
 import org.trustedanalytics.sparktk.frame.internal.ops.flatten.FlattenColumnsTransform
 import org.trustedanalytics.sparktk.frame.internal.ops.RenameColumnsTransform
 import org.trustedanalytics.sparktk.frame.internal.ops.sortedk.SortedKSummarization
@@ -48,6 +48,7 @@ class Frame(frameRdd: RDD[Row], frameSchema: Schema) extends BaseFrame // params
     with EcdfSummarization
     with EntropySummarization
     with ExportToCsvSummarization
+    with ExportToHbaseSummarization
     with ExportToHiveSummarization
     with ExportToJdbcSummarization
     with ExportToJsonSummarization
--- python/sparktk/frame/ops/export_data.py
+++ python/sparktk/frame/ops/export_data.py
@@ -1,4 +1,73 @@
 
+def export_to_hbase(self, table_name, key_column_name=None, family_name="familyColumn"):
+    """
+    Write current frame to HBase table.
+
+    Table must exist in HBase.
+
+    :param self: Current frame to export to hbase table
+    :param table_name: The name of the HBase table that will contain the exported frame
+    :param key_column_name: The name of the column to be used as row key in hbase table
+    :param family_name: The family name of the HBase table that will contain the exported frame
+
+    Example:
+    -----------------------
+
+    <skip>
+
+    >>> from sparktk import TkContext
+    >>> tc=TkContext(sc)
+    >>> data = [[1, 0.2, -2, 5], [2, 0.4, -1, 6], [3, 0.6, 0, 7], [4, 0.8, 1, 8]]
+    >>> schema = [('a', int), ('b', float),('c', int) ,('d', int)]
+    >>> my_frame = tc.to_frame(data, schema)
+    >>> my_frame.export_to_hbase("test_demo_hbase", family_name="test_family")
+    <progress>
+
+    Verify exported frame in hbase
+
+    From bash shell
+
+    $hbase shell
+
+    hbase(main):001:0> list
+
+    You should see test_demo_hbase table.
+
+    Run hbase(main):001:0> scan 'test_demo_hbase' (to verify frame).
+
+    Output:
+    ----------
+        ROW     COLUMN+CELL
+         0      column=test_family:a, timestamp=1464219662295, value=1
+         0      column=test_family:b, timestamp=1464219662295, value=0.2
+         0      column=test_family:c, timestamp=1464219662295, value=-2
+         0      column=test_family:d, timestamp=1464219662295, value=5
+         1      column=test_family:a, timestamp=1464219662295, value=2
+         1      column=test_family:b, timestamp=1464219662295, value=0.4
+         1      column=test_family:c, timestamp=1464219662295, value=-1
+         1      column=test_family:d, timestamp=1464219662295, value=6
+         2      column=test_family:a, timestamp=1464219662295, value=3
+         2      column=test_family:b, timestamp=1464219662295, value=0.6
+         2      column=test_family:c, timestamp=1464219662295, value=0
+         2      column=test_family:d, timestamp=1464219662295, value=7
+         3      column=test_family:a, timestamp=1464219662295, value=4
+         3      column=test_family:b, timestamp=1464219662295, value=0.8
+         3      column=test_family:c, timestamp=1464219662295, value=1
+         3      column=test_family:d, timestamp=1464219662295, value=8
+        4 row(s) in 0.1560 seconds
+
+    </skip>
+    """
+    if not isinstance(table_name, basestring):
+        raise ValueError("Unsupported 'table_name' parameter type.  Expected string, but found %s." % type(table_name))
+
+    if not isinstance(family_name, basestring):
+        raise ValueError(
+            "Unsupported 'family_name' parameter type.  Expected string, but found %s." % type(family_name))
+
+    self._scala.exportToHbase(table_name, self._tc.jutils.convert.to_scala_option(key_column_name), family_name)
+
+
 def export_to_hive(self, hive_table_name):
     """
     Write current frame to Hive table.
