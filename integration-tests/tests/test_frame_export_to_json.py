7151446ee26a7fb0025ed1325033ded2137c35fc
Joyesh Mishra <joyesh.mishra@intel.com>
2016-06-09 16:09:01 -0700

    Naive Bayes (#19)
    * Initial commit
    
    * Add test for Naive Bayes
    
    * refactoring changes

--- integration-tests/tests/test_frame_export_to_json.py
+++ integration-tests/tests/test_frame_export_to_json.py
@@ -4,9 +4,12 @@ import os.path
 import shutil
 import subprocess
 import json
+import logging
+logger = logging.getLogger(__name__)
+
 
 def test_export_to_json_file_path(tc):
-    print "create frame"
+    logger.info("create frame")
     rows = [[1, 0.2, -2, 5], [2, 0.4, -1, 6], [3, 0.6, 0, 7], [4, 0.8, 1, 8]]
     schema = [('a', int), ('b', float),('c', int) ,('d', int)]
     frame = tc.frame.create(rows, schema)
@@ -14,22 +17,22 @@ def test_export_to_json_file_path(tc):
     assert(frame.row_count, 4, "frame should have 4 rows")
     assert(frame.column_names, ['a', 'b', 'c', 'd'])
 
-    print "compute dot product"
+    logger.info("compute dot product")
     frame.dot_product(['a','b'],['c','d'],'dot_product')
     assert(frame.column_names[4], "dot_product", "frame must have last column name as dot_product")
 
-    print "export dp frame in json format to local file system"
+    logger.info("export dp frame in json format to local file system")
     if(os.path.exists("jsonfile12") == True):
         shutil.rmtree("jsonfile12")
     frame.export_to_json("jsonfile12")
     assert(os.path.exists("jsonfile12") == False, "export_to_json should export frame in json format to jsonfile12 folder")
-    print "Removing created file"
+    logger.info("Removing created file")
     shutil.rmtree("jsonfile12")
 
 
 
 def test_strange_strings(tc):
-    print "create frame"
+    logger.info("create frame")
     rows = [["A", "Hi, i'm here"],["B", "Hello's to everyone"],["C", "I'm good ~how are doing@"],["DD", "#$this is something, amazing''s"],['EE', 'He said, "Hello!"'],["FF", "u'It is 15 \u00f8c outside'"]]
     schema = [("name", str), ("message", str)]
     frame = tc.frame.create(rows, schema)
@@ -37,7 +40,7 @@ def test_strange_strings(tc):
     assert(frame.row_count, 6, "frame should have 6 rows")
     assert(frame.column_names, ['name', 'message'])
     dir_name = "sandbox/json_strange_string"
-    print "export frame in json format to local file system"
+    logger.info("export frame in json format to local file system")
     if(os.path.exists(dir_name) == True):
         shutil.rmtree(dir_name)
     frame.export_to_json(dir_name)
@@ -55,5 +58,5 @@ def test_strange_strings(tc):
     json_data2 = json.loads(str(data2))
     assert(json_data2['message'] == frame.take(6).data[5][1], "the value for FF should be u'It is 15 \u00f8c outside'")
 
-    print "Removing created file"
+    logger.info("Removing created file")
     shutil.rmtree(dir_name)
--- python/sparktk/models/classification/__init__.py
+++
@@ -1,1 +0,0 @@
-print "** Loading classification"
