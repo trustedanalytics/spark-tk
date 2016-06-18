885f96ddab4c6092d37e1e45403cbb75f497e91f
Lewis Coates <lewis.coates@intel.com>
2016-06-17 22:33:32 -0700

    Building initial infrastructure for QA Regression suite for spark-tk
    Builds a set of files and scripts for installation of datasets, automatic
    installation of spark-tk from the build server (not from source), execution
    of the test cases, and a sample test case of confusion matrix to prove
    the system is operational.
    
    Also includes a set of development scripts in order to facilitate QA
    test writing.

---
+++ regression-tests/utils/home.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+NAME="[`basename ${BASH_SOURCE[0]}`]"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# echo "$NAME DIR=$DIR"
+
+MAINDIR="$(dirname $DIR)"
+MAINDIR="$(dirname $MAINDIR)"
+
+export SPARKTK_HOME=$MAINDIR/core/target
---
+++ regression-tests/utils/install_source.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+NAME="[`basename $BASH_SOURCE[0]`]"
+DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
+echo "$NAME DIR=$DIR"
+
+MAINDIR="$(dirname $DIR)"
+MAINDIR="$(dirname $MAINDIR)"
+
+echo "Uninstalling spark_tk"
+sudo pip2.7 uninstall sparktk
+
+echo "installing spark_tk"
+sudo pip2.7 install $MAINDIR/python/dist/*.gz
+
+echo "linking pyspark"
+sudo ln -fs /opt/cloudera/parcels/CDH/lib/spark/python/pyspark /usr/lib/python2.7/site-packages/
+
+echo "inflating jars"
+pushd $MAINDIR/core/target
+unzip *.zip
+popd
+echo "list jars"
+ls $MAINDIR/core/target
