23d2dbc955594ee1a37394d4bf347c3d232d3265
Lewis Coates <lewis.coates@intel.com>
2016-06-30 14:21:42 -0700

    add new tests, update test infrastructure
    Adds new tests for the three column methods (add column, rename column
    drop column), split into three files.
    
    Updates test infrastructure to handle multiple test case files
    
    Updates test infrastructure to use py.test

--- regression-tests/qalib/sparktk_test.py
+++ regression-tests/qalib/sparktk_test.py
@@ -1,10 +1,25 @@
-# vim: set encoding=utf-8 
 """Setup up tests for regression """
 import unittest
+import uuid
+import datetime
 
 import sparktk as stk
 
 import config
+
+# set up a singleton to share spark context across multiple test suites
+# each py.test run can only run create context 1 time
+global_spark_context = None
+spark_context_initialized = False
+
+
+def get_context():
+    global global_spark_context
+    global spark_context_initialized
+    if not spark_context_initialized:
+        global_spark_context = stk.TkContext()
+        spark_context_initialized = True
+    return global_spark_context
 
 
 class SparkTKTestCase(unittest.TestCase):
@@ -12,7 +27,7 @@ class SparkTKTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """Build the context for use"""
-        cls.context = stk.TkContext()
+        cls.context = get_context()
 
     def setUp(self):
         pass
@@ -27,7 +42,6 @@ class SparkTKTestCase(unittest.TestCase):
     def get_file(self, filename):
         placed_path = "/user/" + config.user + "/qa_data/" + filename
         return placed_path
-
 
     def get_name(self, prefix):
         """build a guid hardened unique name """
