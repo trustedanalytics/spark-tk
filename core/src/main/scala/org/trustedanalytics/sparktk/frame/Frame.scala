8c1bd380658767118595016d01d79f7549f5c3f1
Shibani Singh <shibani.singh@intel.com>
2016-07-07 11:08:31 -0700

    Dpng 8168 (#52)
    * beginning power iteration code
    
    * power iteration done
    
    * Added comments to power iteration
    
    * updated
    
    * updated again
    
    * power iteration updated
    
    * power iteration modified.
    
    * minor change
    
    * add power iteration integration test

--- core/src/main/scala/org/trustedanalytics/sparktk/frame/Frame.scala
+++ core/src/main/scala/org/trustedanalytics/sparktk/frame/Frame.scala
@@ -5,7 +5,7 @@ import org.apache.spark.api.java.JavaRDD
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
 import org.json4s.JsonAST.JValue
-import org.trustedanalytics.sparktk.frame.internal.{ ValidationReport, BaseFrame }
+import org.trustedanalytics.sparktk.frame.internal.{ BaseFrame, ValidationReport }
 import org.trustedanalytics.sparktk.frame.internal.ops._
 import org.trustedanalytics.sparktk.frame.internal.ops.binning.{ BinColumnTransformWithResult, HistogramSummarization, QuantileBinColumnTransformWithResult }
 import org.trustedanalytics.sparktk.frame.internal.ops.classificationmetrics.{ BinaryClassificationMetricsSummarization, MultiClassClassificationMetricsSummarization }
@@ -16,6 +16,7 @@ import org.trustedanalytics.sparktk.frame.internal.ops.exportdata.{ ExportToCsvS
 import org.trustedanalytics.sparktk.frame.internal.ops.flatten.FlattenColumnsTransform
 import org.trustedanalytics.sparktk.frame.internal.ops.groupby.GroupBySummarization
 import org.trustedanalytics.sparktk.frame.internal.ops.RenameColumnsTransform
+import org.trustedanalytics.sparktk.frame.internal.ops.poweriterationclustering.PowerIterationClusteringSummarization
 import org.trustedanalytics.sparktk.frame.internal.ops.sortedk.SortedKSummarization
 import org.trustedanalytics.sparktk.frame.internal.ops.statistics.correlation.{ CorrelationMatrixSummarization, CorrelationSummarization }
 import org.trustedanalytics.sparktk.frame.internal.ops.statistics.covariance.{ CovarianceMatrixSummarization, CovarianceSummarization }
@@ -63,6 +64,7 @@ class Frame(frameRdd: RDD[Row], frameSchema: Schema, validateSchema: Boolean = f
     with JoinOuterSummarization
     with JoinRightSummarization
     with MultiClassClassificationMetricsSummarization
+    with PowerIterationClusteringSummarization
     with QuantilesSummarization
     with QuantileBinColumnTransformWithResult
     with RenameColumnsTransform
