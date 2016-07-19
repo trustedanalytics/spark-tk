ccb5ce41a4d9a693c6a6f5fa49ebe8f3f0c0d53e
aayushidwivedi01 <aayushi.dwivedi@intel.com>
2016-07-19 15:30:10 -0700

    Removed header from cumsum_doc_test

--- regression-tests/testcases/models/cumsum_doc_test.py
+++ regression-tests/testcases/models/cumsum_doc_test.py
@@ -1,28 +1,6 @@
-##############################################################################
-# INTEL CONFIDENTIAL
-#
-# Copyright 2014, 2015 Intel Corporation All Rights Reserved.
-#
-# The source code contained or described herein and all documents related to
-# the source code (Material) are owned by Intel Corporation or its suppliers
-# or licensors. Title to the Material remains with Intel Corporation or its
-# suppliers and licensors. The Material may contain trade secrets and
-# proprietary and confidential information of Intel Corporation and its
-# suppliers and licensors, and is protected by worldwide copyright and trade
-# secret laws and treaty provisions. No part of the Material may be used,
-# copied, reproduced, modified, published, uploaded, posted, transmitted,
-# distributed, or disclosed in any way without Intel's prior express written
-# permission.
-#
-# No license under any patent, copyright, trade secret or other intellectual
-# property right is granted to or conferred upon you by disclosure or
-# delivery of the Materials, either expressly, by implication, inducement,
-# estoppel or otherwise. Any license under such intellectual property rights
-# must be express and approved by Intel in writing.
-##############################################################################
-""" usage: python2.7 naive_bayes_doc_test.py
+""" usage: python2.7 cumsum_doc_test.py
 
-    THIS TEST REQUIRES NO THIRD PARTY APPLICATIONS OTHER THAN THE ATK
+    THIS TEST REQUIRES NO THIRD PARTY APPLICATIONS OTHER THAN THE SPARKTK
     THIS TEST IS TO BE MAINTAINED AS A SMOKE TEST FOR THE ML SYSTEM
 """
 import unittest
