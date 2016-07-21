a3801a8478f61dde66dabab5db366640e345a9e8
aayushidwivedi01 <aayushi.dwivedi@intel.com>
2016-07-21 10:06:15 -0700

    Cleaned up the scripts

--- regression-tests/testcases/frames/cumsum_doc_test.py
+++ regression-tests/testcases/frames/cumsum_doc_test.py
@@ -1,6 +1,4 @@
-""" usage: python2.7 cumsum_doc_test.py
-
-    THIS TEST REQUIRES NO THIRD PARTY APPLICATIONS OTHER THAN THE SPARKTK
+""" THIS TEST REQUIRES NO THIRD PARTY APPLICATIONS OTHER THAN THE SPARKTK
     THIS TEST IS TO BE MAINTAINED AS A SMOKE TEST FOR THE ML SYSTEM
 """
 import unittest
@@ -14,21 +12,16 @@ class CumSumTest(unittest.TestCase):
 
         # The general workflow will be build a frame, run some analytics
         # on the frame
-
         # First Step, construct a frame
         # Construct a frame to be uploaded, this is done using plain python
         # lists uploaded to the server
-
         # The following frame could represent some ordered list (such as
         # customer orders) and a value associated with the order.
         # The order is sorted on, and then the order value is accumulated
-
         # Cumulative sum finds the sum up to and including a given order
 	
         # Create context 
         tc = TkContext()
-
-
         # Create the frame using a list object
         frame = tc.frame.create(data=[[0,100],
                                       [3,20],
@@ -36,21 +29,13 @@ class CumSumTest(unittest.TestCase):
                                       [2,90]],
                                 schema=[("order",int),
                                         ("value", int)])
-
         print frame.inspect()
-
         # Sort on order, note this is a side effect based operation
-
         frame.sort('order')
-
         # calculate the cumulative sum
-
         frame.cumulative_sum('value')
-        
         print frame.inspect()
-
         # Fetch the results, and validate they are what you would expect
-
         result = frame.take(frame.row_count)
         self.assertItemsEqual(
             result.data, [[0,100,100], [3,20,235], [1,25,125], [2,90,215]])
@@ -58,5 +43,3 @@ class CumSumTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-
