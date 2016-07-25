8680d1a65e05f1c6c93c4c9b02a9b0905846b110
Lewis Coates <lewis.coates@intel.com>
2016-07-25 16:17:05 -0700

    fixes some more issues from the code review

--- regression-tests/testcases/frames/column_method_add_test.py
+++ regression-tests/testcases/frames/column_method_add_test.py
@@ -7,22 +7,22 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath
 
 from qalib import sparktk_test
 
-dummy_int_val = -77     # placeholder data value for added column
-dummy_col_count = 1000  # length of dummy list for column add
+udf_int_val = -77     # placeholder data value for added column
+udf_col_count = 1000  # length of list for column add
 
 
 # This method is to test different sources of functions
 # i.e. global
-def global_dummy_val_list(row):
-    return [dummy_int_val for _ in range(0, dummy_col_count)]
+def global_udf(row):
+    return [udf_int_val for _ in range(0, udf_col_count)]
 
 
 class ColumnMethodTest(sparktk_test.SparkTKTestCase):
 
     # Test class bound methods
     @staticmethod
-    def static_dummy_val_list(row):
-        return [dummy_int_val for _ in range(0, dummy_col_count)]
+    def static_udf(row):
+        return [udf_int_val for _ in range(0, udf_col_count)]
 
     def setUp(self):
         """Build test_frame"""
@@ -31,27 +31,27 @@ class ColumnMethodTest(sparktk_test.SparkTKTestCase):
         schema = [("int", int), ("str", str), ("float", float)]
 
         self.frame = self.context.frame.import_csv(dataset, schema=schema)
+        old_header = self.frame.column_names
+        self.new_col_schema = [("col_" + str(n), int)
+                               for n in range(0, udf_col_count)]
+        self.expected_header = old_header + [col_schema[0]
+                                             for col_schema in 
+                                                self.new_col_schema]
 
     def test_static_add_col_names(self):
         """Tests adding a column name with a static method"""
-        old_header = self.frame.column_names
-        new_col_schema = [("col_" + str(n), int)
-                          for n in range(0, dummy_col_count)]
         self.frame.add_columns(
-            ColumnMethodTest.static_dummy_val_list, new_col_schema)
-        expected_header = old_header + [col_schema[0]
-                                        for col_schema in new_col_schema]
-        self.assertEqual(self.frame.column_names, expected_header)
+            ColumnMethodTest.static_udf, self.new_col_schema)
+        self.assertEqual(self.frame.column_names, self.expected_header)
+
+        columns = self.frame.take(self.frame.row_count).data
+        for i in columns:
+            self.assertEqual(i[-1], udf_int_val)
 
     def test_add_col_names(self):
         """Tests adding a column name with a global method"""
-        old_header = self.frame.column_names
-        new_col_schema = [("col_" + str(n), int)
-                          for n in range(0, dummy_col_count)]
-        self.frame.add_columns(global_dummy_val_list, new_col_schema)
-        expected_header = old_header + [col_schema[0]
-                                        for col_schema in new_col_schema]
-        self.assertEqual(self.frame.column_names, expected_header)
+        self.frame.add_columns(global_udf, self.new_col_schema)
+        self.assertEqual(self.frame.column_names, self.expected_header)
 
     def test_add_columns_lambda_single(self):
         """Test adding individual columns from a lambda"""
@@ -152,51 +152,52 @@ class ColumnMethodTest(sparktk_test.SparkTKTestCase):
     def test_add_columns_add_existing_name(self):
         """Test adding columns with existing names errors"""
         with self.assertRaises(ValueError):
-            self.frame.add_columns(lambda row: dummy_int_val, ('str', int))
+            self.frame.add_columns(lambda row: udf_int_val, ('str', int))
             self.frame.inspect()
 
     @unittest.skip("column names not validated")
     def test_add_column_with_empty_name(self):
         """Test adding a column with an empty name errors"""
         with self.assertRaises(ValueError):
-            self.frame.add_columns(lambda row: dummy_int_val, ('', int))
+            self.frame.add_columns(lambda row: udf_int_val, ('', int))
             self.frame.inspect()
 
     @unittest.skip("column names not validated")
     def test_add_column_null_schema_no_force(self):
         """Test adding a column with a null schema errors, don't force eval"""
         with self.assertRaises(TypeError):
-            self.frame.add_columns(lambda row: dummy_int_val, None)
+            self.frame.add_columns(lambda row: udf_int_val, None)
 
     @unittest.skip("column names not validated")
     def test_add_column_empty_schema_no_force(self):
         """Test adding a column with empty schema errors, don't force eval"""
         with self.assertRaises(ValueError):
-            self.frame.add_columns(lambda row: dummy_int_val, ())
+            self.frame.add_columns(lambda row: udf_int_val, ())
 
     def test_add_column_null_schema(self):
         """Test adding a column with a null schema errors"""
-        with self.assertRaisesRegexp(TypeError, "'NoneType' object has no attribute '__getitem__'"):
-            self.frame.add_columns(lambda row: dummy_int_val, None)
+        with self.assertRaisesRegexp(TypeError,
+            "'NoneType' object has no attribute '__getitem__'"):
+            self.frame.add_columns(lambda row: udf_int_val, None)
             self.frame.inspect()
 
     def test_add_column_empty_schema(self):
         """Test adding a column with an empty schema errors"""
         with self.assertRaisesRegexp(IndexError, "tuple index out of range"):
-            self.frame.add_columns(lambda row: dummy_int_val, ())
+            self.frame.add_columns(lambda row: udf_int_val, ())
             self.frame.inspect()
 
     def test_add_column_schema_list(self):
         """Test adding a column with a schema containing a list"""
         with self.assertRaisesRegexp(Exception, 'concatenate list'):
             self.frame.add_columns(
-                    lambda row: dummy_int_val, schema=[('new_col', int)])
+                    lambda row: udf_int_val, schema=[('new_col', int)])
             self.frame.inspect()
         
     def test_unicode_conversion(self):
         """Test renaming with unicode names"""
         self.frame.add_columns(
-            lambda row: dummy_int_val, ('product', int))
+            lambda row: udf_int_val, ('product', int))
         col_count = len(self.frame.take(1).data[0])
         self.frame.rename_columns({'product': u'unicode'})
         self.assertEqual(col_count, len(self.frame.take(1)[0][0]))
