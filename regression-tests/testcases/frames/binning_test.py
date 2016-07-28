082f220e97ca29dc1eb0b4fb4e59756b67070626
aayushidwivedi01 <aayushi.dwivedi@intel.com>
2016-07-28 13:33:35 -0700

    Added dataset for binning

--- regression-tests/testcases/frames/binning_test.py
+++ regression-tests/testcases/frames/binning_test.py
@@ -1,13 +1,13 @@
 """ Test Binning against a separate implemenation """
 
 import unittest
-
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
-
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(
+    os.path.realpath(__file__)))))
 from qalib import sparktk_test
 from sparktk import dtypes
+
 
 class BinningHarness(sparktk_test.SparkTKTestCase):
 
@@ -23,99 +23,133 @@ class BinningHarness(sparktk_test.SparkTKTestCase):
         # Movie user data with original ratings
         # Big data frame from data with 5 ratings
         self.frame_5ratings = self.context.frame.import_csv(
-            self.get_file("netf_5_ratings.csv"), schema=self.schema)
+            self.get_file("movie_user_5ratings.csv"), schema=self.schema)
 
         # Movie user data with some missing ratings
         # Big data frame from data with only 1 ratings
         self.frame_1ratings = self.context.frame.import_csv(
-            self.get_file("netf_all_3.csv"), schema=self.schema)
+            self.get_file("movie_user_1rating.csv"), schema=self.schema)
 
     def test_equalwidth_5bins_5ratings(self):
         """Equal width binning on 5 ratings into 5 bin"""
-        baseline = [[0, 3970], [1, 3744], [2, 3994], [3, 3950], [4, 3944]]
+        baseline = [[0, 3926L], [1, 3974L], [2, 3792L], [3, 3988L], [4, 3922L]]
         self.frame_5ratings.bin_column("rating", 5, bin_column_name="binned0")
-        groupby_frame = self.frame_5ratings.group_by("binned0", self.context.agg.count)
-        self.assertItemsEqual( sorted(groupby_frame.take(10).data), baseline)
+        groupby_frame = self.frame_5ratings.group_by(
+            "binned0", self.context.agg.count)
+        self.assertItemsEqual(sorted(groupby_frame.take(10).data), baseline)
 
     def test_equalwidth_2bins_5ratings(self):
         """Equal width  binning on 5 ratings into 2 bins"""
-        baseline = [[0, 7714], [1, 11888]]
+        baseline = [[0, 7900L], [1, 11702L]]
         self.frame_5ratings.bin_column("rating", 2, bin_column_name="binned1")
-        groupby_frame = self.frame_5ratings.group_by("binned1", self.context.agg.count)
-        self.assertItemsEqual( sorted(groupby_frame.take(10).data), baseline)
+        groupby_frame = self.frame_5ratings.group_by(
+            "binned1", self.context.agg.count)
+
+        self.assertItemsEqual(sorted(groupby_frame.take(10).data), baseline)
 
     def test_equalwidth_10bins_5ratings(self):
         """Equal Width binning on 5 ratings and 10 bins"""
-        baseline = [[0, 3970], [2, 3744], [5, 3994], [7, 3950], [9, 3944]]
+        baseline = [[0, 3926L], [2, 3974L], [5, 3792L], [7, 3988L], [9, 3922L]]
         self.frame_5ratings.bin_column("rating", 10, bin_column_name="binned2")
-        groupby_frame = self.frame_5ratings.group_by("binned2", self.context.agg.count)
-        self.assertItemsEqual( sorted(groupby_frame.take(10).data), baseline)
+        groupby_frame = self.frame_5ratings.group_by(
+            "binned2", self.context.agg.count)
+
+        self.assertItemsEqual(sorted(groupby_frame.take(10).data), baseline)
 
     def test_equaldepth_5bins_5ratings(self):
         """Equal depth binning on 5 ratings into 5 bins"""
-        baseline = [[0, 3970], [1, 3744], [2, 3994], [3, 3950], [4, 3944]]
-        self.frame_5ratings.quantile_bin_column("rating", 5, bin_column_name="binned3")
-        groupby_frame = self.frame_5ratings.group_by("binned3", self.context.agg.count)
-        self.assertItemsEqual( sorted(groupby_frame.take(10).data), baseline)
+        baseline = [[0, 3926L], [1, 3974L], [2, 3792L], [3, 3988L], [4, 3922L]]
+        self.frame_5ratings.quantile_bin_column(
+            "rating", 5, bin_column_name="binned3")
+        groupby_frame = self.frame_5ratings.group_by(
+            "binned3", self.context.agg.count)
+
+        self.assertItemsEqual(sorted(groupby_frame.take(10).data), baseline)
 
     def test_equaldepth_2bins_5ratings(self):
         """Equal depth binning on 5 ratings into 2 bins"""
-        baseline = [[0, 11708], [1, 7894]]
-        self.frame_5ratings.quantile_bin_column("rating", 2, bin_column_name="binned4")
-        groupby_frame = self.frame_5ratings.group_by("binned4", self.context.agg.count)
-        self.assertItemsEqual( sorted(groupby_frame.take(10).data), baseline)
+        baseline = [[0, 11692L], [1, 7910L]]
+        self.frame_5ratings.quantile_bin_column(
+            "rating", 2, bin_column_name="binned4")
+        groupby_frame = self.frame_5ratings.group_by(
+            "binned4", self.context.agg.count)
+
+        self.assertItemsEqual(sorted(groupby_frame.take(10).data), baseline)
 
     def test_equaldepth_10bins_5ratings(self):
         """Equal depth binning on 5 ratings into 10 bins """
-        self.frame_5ratings.quantile_bin_column("rating", 10, bin_column_name="binned5")
-        baseline = [[0, 3970], [1, 3744], [2, 3994], [3, 3950], [4, 3944]]
-        groupby_frame = self.frame_5ratings.group_by("binned5", self.context.agg.count)
-        self.assertItemsEqual( sorted(groupby_frame.take(10).data), baseline)
+        baseline = [[0, 3926L], [1, 3974L], [2, 3792L], [3, 3988L], [4, 3922L]]
+        self.frame_5ratings.quantile_bin_column(
+            "rating", 10, bin_column_name="binned5")
+        groupby_frame = self.frame_5ratings.group_by(
+            "binned5", self.context.agg.count)
+
+        self.assertItemsEqual(sorted(groupby_frame.take(10).data), baseline)
 
     def test_equalwidth_5bins_1rating(self):
         """Equal width binning on 1 rating and 5 bins"""
+        baseline = [[0, 19602L]]
         self.frame_1ratings.bin_column("rating", 5, bin_column_name="binned6")
-        baseline = [[0, 19602]]
-        groupby_frame = self.frame_1ratings.group_by("binned6", self.context.agg.count)
-        self.assertEqual( sorted(groupby_frame.take(10).data), baseline)
+        groupby_frame = self.frame_1ratings.group_by(
+            "binned6", self.context.agg.count)
+
+        self.assertEqual(sorted(groupby_frame.take(10).data), baseline)
 
     def test_equaldepth_5bins_1rating(self):
         """Equal depth binning on 1 rating and 5 bins"""
-        self.frame_1ratings.quantile_bin_column("rating", 5, bin_column_name="binned7")
-        baseline = [[0, 19602]]
-        groupby_frame = self.frame_1ratings.group_by("binned7", self.context.agg.count)
-        self.assertEqual( sorted(groupby_frame.take(10).data), baseline)
+        baseline = [[0, 19602L]]
+        self.frame_1ratings.quantile_bin_column(
+            "rating", 5, bin_column_name="binned7")
+        groupby_frame = self.frame_1ratings.group_by(
+            "binned7", self.context.agg.count)
+
+        self.assertEqual(sorted(groupby_frame.take(10).data), baseline)
 
     def test_equaldepth_2bins_1_2_5_rating(self):
         """Equal depth binning on ratings 1, 2 and 5 into 2 bins"""
         # Movie user data with some missing ratings
         # Big data frame from data with ratings 1, 2 and 5 only.
-        netf_1_2_5 = self.get_file("netf_1_2_5.csv")
+        baseline = [[0, 12992L], [1, 6610L]]
         frame_1_2_5_ratings = self.context.frame.import_csv(
-            self.get_file("netf_1_2_5.csv"), schema = self.schema)
+            self.get_file("movie_user_3ratings.csv"), schema=self.schema)
 
-        frame_1_2_5_ratings.quantile_bin_column("rating", 2, bin_column_name="binned8")
-        baseline = [[0, 7714], [1, 11888]]
-        groupby_frame = frame_1_2_5_ratings.group_by("binned8", self.context.agg.count)
-        self.assertEqual( sorted(groupby_frame.take(10).data), baseline)
+        frame_1_2_5_ratings.quantile_bin_column(
+            "rating", 2, bin_column_name="binned8")
+        groupby_frame = frame_1_2_5_ratings.group_by(
+            "binned8", self.context.agg.count)
+
+        self.assertEqual(sorted(groupby_frame.take(10).data), baseline)
 
     def test_bin_cutoffs_strict_binning_true_4bins_5ratings(self):
         """Binning using cutoffs on 5 ratings and 2 bins"""
-        self.frame_5ratings.bin_column("rating", [1, 2, 3, 4], include_lowest=False,
-            strict_binning=True, bin_column_name="binned9")
-        baseline = [[-1, 3944], [0, 7714], [1, 3994], [2, 3950]]
-        groupby_frame = self.frame_5ratings.group_by("binned9", self.context.agg.count)
-        self.assertEqual( sorted(groupby_frame.take(10).data), baseline)
+        baseline = [[-1, 3922L], [0, 7900L], [1, 3792L], [2, 3988L]]
+        self.frame_5ratings.bin_column(
+            "rating", [1, 2, 3, 4],
+            include_lowest=False,
+            strict_binning=True,
+            bin_column_name="binned9")
+        groupby_frame = self.frame_5ratings.group_by(
+            "binned9", self.context.agg.count)
+
+        self.assertEqual(sorted(groupby_frame.take(10).data), baseline)
 
     def test_bin_cutoff_strict_binning_false_3bins_5ratings(self):
         """Binning using cutoff with strict binning false"""
-        self.frame_5ratings.bin_column("rating", [1, 2, 3, 4], include_lowest=False,
-            strict_binning=False, bin_column_name="binned10")
-        baseline = [[0, 7714], [1,3994], [2, 7894]]
-        groupby_frame = self.frame_5ratings.group_by("binned10", self.context.agg.count)
-        self.assertEqual( sorted( groupby_frame.take(10).data), baseline)
+        baseline = [[0, 7900L], [1, 3792L], [2, 7910L]]
+        self.frame_5ratings.bin_column(
+            "rating", [1, 2, 3, 4],
+            include_lowest=False,
+            strict_binning=False,
+            bin_column_name="binned10")
+        groupby_frame = self.frame_5ratings.group_by(
+            "binned10", self.context.agg.count)
+
+        self.assertEqual(sorted(groupby_frame.take(10).data), baseline)
+
+    def test_negative_bin_number(self):
+        with self.assertRaises(Exception):
+            self.frame_5ratings.bin_column(
+            "rating", -1)
 
 if __name__ == '__main__':
     unittest.main()
-
-
