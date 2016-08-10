e2d9fc193bc7d4007b8d04166820cdec5edf853d
Haley Bourke <haleyobourke@yahoo.com>
2016-08-10 15:32:23 -0700

    refactors multiclass classification test

--- regression-tests/sparktkregtests/testcases/frames/frame_multiclass_classification_test.py
+++ regression-tests/sparktkregtests/testcases/frames/frame_multiclass_classification_test.py
@@ -1,23 +1,21 @@
 """ tests multiclass classification metrics"""
 
 import unittest
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
-from qalib import sparktk_test
+from sparktkregtests.lib import sparktk_test
 
 
 class ClassificationMetrics(sparktk_test.SparkTKTestCase):
-
 
     def test_multinomial_frame_classification(self):
         """Tests the confusion matrix functionality"""
         schema = [("actual", int), ("predicted", int), ("count", int)]
         class_csv = self.get_file("class_data.csv") # the name of our data file, needs to be in hdfs for the test to run
-        actual_result = [5, 2, 3, 0, 5, 3, 0, 2, 5] # the values we expect to get from our test
         frame = self.context.frame.import_csv(class_csv, schema=schema) # uploading our data file, will return a frame
+
+        actual_result = [5, 2, 3, 0, 5, 3, 0, 2, 5] # the values we expect to get from our test
+
         classMetrics = frame.multiclass_classification_metrics('actual', 'predicted', frequency_column='count') # the label column, result column, and frequency column are the params
-        conf_matrix = classMetrics.confusion_matrix.values 
+        conf_matrix = classMetrics.confusion_matrix.values
         cumulative_matrix_list = [conf_matrix[0][0],
                                   conf_matrix[0][1],
                                   conf_matrix[0][2],
@@ -27,7 +25,9 @@ class ClassificationMetrics(sparktk_test.SparkTKTestCase):
                                   conf_matrix[2][0],
                                   conf_matrix[2][1],
                                   conf_matrix[2][2]]
+
         self.assertEqual(actual_result, cumulative_matrix_list)
+
 
 if __name__ == '__main__':
     unittest.main()
