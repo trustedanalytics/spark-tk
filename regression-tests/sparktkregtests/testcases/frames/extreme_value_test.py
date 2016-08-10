554a6e2e6fea363875836767a0dfef9e87c73d29
aayushidwivedi01 <aayushi.dwivedi@intel.com>
2016-08-10 10:17:26 -0700

    Modifies qalib

--- regression-tests/sparktkregtests/testcases/frames/extreme_value_test.py
+++ regression-tests/sparktkregtests/testcases/frames/extreme_value_test.py
@@ -4,8 +4,7 @@ import numpy as np
 import math
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
-from qalib import sparktk_test
+from sparktkregtests.lib import sparktk_test
 from sparktk import dtypes
 
 
