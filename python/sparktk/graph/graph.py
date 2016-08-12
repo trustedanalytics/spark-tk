ff12a8168c25ec4b9a6f6e1c741a8cae9f08b14d
Lewis Coates <lewis.coates@intel.com>
2016-08-12 19:28:26 +0000

    adds weighted degree for graph frames, adds the py.test junit xml to .gitignore

--- python/sparktk/graph/graph.py
+++ python/sparktk/graph/graph.py
@@ -259,3 +259,4 @@ class Graph(object):
     # Graph Operations
     from sparktk.graph.ops.save import save
     from sparktk.graph.ops.vertex_count import vertex_count
+    from sparktk.graph.ops.weighted_degrees import weighted_degrees
---
+++ sparktk-core/src/test/scala/org/trustedanalytics/sparktk/graph/internal/ops/WeightedDegreesTest.scala
@@ -0,0 +1,102 @@
+package org.trustedanalytics.sparktk.graph.internal.ops
+
+import org.scalatest.{ WordSpec, Matchers }
+import org.trustedanalytics.sparktk.frame.{ Frame, DataTypes, Column, FrameSchema }
+import org.trustedanalytics.sparktk.frame.internal.rdd.FrameRdd
+import org.trustedanalytics.sparktk.graph.{ Graph }
+import org.apache.spark.sql.catalyst.expressions.GenericRow
+import org.trustedanalytics.sparktk.testutils.TestingSparkContextWordSpec
+
+class WeightedDegreeTest extends TestingSparkContextWordSpec with Matchers {
+
+  "weighted degree" when {
+    "set with outdegree" should {
+      "sum the properties going in" in {
+        val vertices = FrameSchema(List(Column("id", DataTypes.int32)))
+        // This graph is a diamond, 1 to 3 and 4, 3 and 4 to 5
+        val vertexRows = FrameRdd.toRowRDD(vertices, sparkContext.parallelize(List(Array[Any](1), Array[Any](3), Array[Any](4), Array[Any](5))))
+        val edges = FrameSchema(List(Column("src", DataTypes.int32), Column("dst", DataTypes.int32), Column("count", DataTypes.int32)))
+        val edgeRows = FrameRdd.toRowRDD(edges, sparkContext.parallelize(
+          List(Array[Any](1, 3, 2),
+            Array[Any](1, 4, 3),
+            Array[Any](3, 5, 7),
+            Array[Any](4, 5, 11))))
+
+        val edgeFrame = new Frame(edgeRows, edges)
+        val vertexFrame = new Frame(vertexRows, vertices)
+        val graph = new Graph(vertexFrame, edgeFrame)
+        val weightedDegree = graph.weightedDegree("count", "in", 0)
+        println(weightedDegree.schema.columns.toString)
+        weightedDegree.schema.columns should equal(List(Column("Node", DataTypes.int32), Column("Degree", DataTypes.int64)))
+        weightedDegree.rdd.toArray.toList should equal(List(new GenericRow(Array[Any](1, 0)), new GenericRow(Array[Any](3, 2)), new GenericRow(Array[Any](5, 18)), new GenericRow(Array[Any](4, 3))))
+
+      }
+    }
+
+    "set with outdegree" should {
+      "sum the properties going out" in {
+        val vertices = FrameSchema(List(Column("id", DataTypes.int32)))
+        // This graph is a diamond, 1 to 3 and 4, 3 and 4 to 5
+        val vertexRows = FrameRdd.toRowRDD(vertices, sparkContext.parallelize(List(Array[Any](1), Array[Any](3), Array[Any](4), Array[Any](5))))
+        val edges = FrameSchema(List(Column("src", DataTypes.int32), Column("dst", DataTypes.int32), Column("count", DataTypes.int32)))
+        val edgeRows = FrameRdd.toRowRDD(edges, sparkContext.parallelize(
+          List(Array[Any](1, 3, 2),
+            Array[Any](1, 4, 3),
+            Array[Any](3, 5, 7),
+            Array[Any](4, 5, 11))))
+
+        val edgeFrame = new Frame(edgeRows, edges)
+        val vertexFrame = new Frame(vertexRows, vertices)
+        val graph = new Graph(vertexFrame, edgeFrame)
+        val weightedDegree = graph.weightedDegree("count", "out", 0)
+        println(weightedDegree.schema.columns.toString)
+        weightedDegree.schema.columns should equal(List(Column("Node", DataTypes.int32), Column("Degree", DataTypes.int64)))
+        weightedDegree.rdd.toArray.toList should equal(List(new GenericRow(Array[Any](1, 5)), new GenericRow(Array[Any](3, 7)), new GenericRow(Array[Any](5, 0)), new GenericRow(Array[Any](4, 11))))
+
+      }
+    }
+
+    "set with undirected" should {
+      "sum the properties going in or out" in {
+        val vertices = FrameSchema(List(Column("id", DataTypes.int32)))
+        // This graph is a diamond, 1 to 3 and 4, 3 and 4 to 5
+        val vertexRows = FrameRdd.toRowRDD(vertices, sparkContext.parallelize(List(Array[Any](1), Array[Any](3), Array[Any](4), Array[Any](5))))
+        val edges = FrameSchema(List(Column("src", DataTypes.int32), Column("dst", DataTypes.int32), Column("count", DataTypes.int32)))
+        val edgeRows = FrameRdd.toRowRDD(edges, sparkContext.parallelize(
+          List(Array[Any](1, 3, 2),
+            Array[Any](1, 4, 3),
+            Array[Any](3, 5, 7),
+            Array[Any](4, 5, 11))))
+
+        val edgeFrame = new Frame(edgeRows, edges)
+        val vertexFrame = new Frame(vertexRows, vertices)
+        val graph = new Graph(vertexFrame, edgeFrame)
+        val weightedDegree = graph.weightedDegree("count", "undirected", 0)
+        println(weightedDegree.schema.columns.toString)
+        weightedDegree.schema.columns should contain theSameElementsInOrderAs List(Column("Node", DataTypes.int32), Column("Degree", DataTypes.int64))
+        weightedDegree.rdd.toArray.toList should contain theSameElementsAs List(new GenericRow(Array[Any](1, 5)), new GenericRow(Array[Any](3, 9)), new GenericRow(Array[Any](5, 18)), new GenericRow(Array[Any](4, 14)))
+
+      }
+    }
+
+    "called with an invalid string" should {
+      "throw an exception" in {
+        val vertices = FrameSchema(List(Column("id", DataTypes.int32)))
+        // This graph is a diamond, 1 to 3 and 4, 3 and 4 to 5
+        val vertexRows = FrameRdd.toRowRDD(vertices, sparkContext.parallelize(List(Array[Any](1), Array[Any](3), Array[Any](4), Array[Any](5))))
+        val edges = FrameSchema(List(Column("src", DataTypes.int32), Column("dst", DataTypes.int32), Column("count", DataTypes.int32)))
+        val edgeRows = FrameRdd.toRowRDD(edges, sparkContext.parallelize(
+          List(Array[Any](1, 3, 2),
+            Array[Any](1, 4, 3),
+            Array[Any](3, 5, 7),
+            Array[Any](4, 5, 11))))
+
+        val edgeFrame = new Frame(edgeRows, edges)
+        val vertexFrame = new Frame(vertexRows, vertices)
+        val graph = new Graph(vertexFrame, edgeFrame)
+        an[IllegalArgumentException] should be thrownBy graph.weightedDegree("count", "invalid", 0)
+      }
+    }
+
+  }
+}
