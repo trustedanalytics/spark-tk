df6336fe32555f0104ac678a5ac4e92d7a80d5c9
Haley Bourke <haleyobourke@yahoo.com>
2016-08-19 14:10:01 -0700

    adds data file for covar/corr tests and refactors

--- regression-tests/sparktkregtests/testcases/frames/covariance_correlation_test.py
+++ regression-tests/sparktkregtests/testcases/frames/covariance_correlation_test.py
@@ -9,38 +9,52 @@ class CovarianceCorrelationTest(sparktk_test.SparkTKTestCase):
     def setUp(self):
         """Build test frames"""
         super(CovarianceCorrelationTest, self).setUp()
-
-        data_in = self.get_file("Covariance_400Col_1K.02_without_spaces.csv")
+        data_in = self.get_file("covariance_correlation.csv")
         self.base_frame = self.context.frame.import_csv(data_in)
 
     def test_covar(self):
         """Test covariance between 2 columns"""
-        covar_2_5 = self.base_frame.covariance('C3','C6')
-        self.assertEqual(covar_2_5, -5.25513196480937949673)
+        sparktk_result = self.base_frame.covariance('C1','C4')
+        C1_C4_columns_data = self.base_frame.take(self.base_frame.row_count, columns=["C1", "C4"]).data
+        numpy_result = numpy.cov(list(C1_C4_columns_data), rowvar=False)
+
+        self.assertAlmostEqual(sparktk_result, float(numpy_result[0][1]))
 
     def test_correl(self):
         """Test correlation between 2 columns"""
-        correl_1_3 = self.base_frame.correlation('C2', 'C4')
-        self.assertEqual(correl_1_3, 0.124996244847393425669857)
+        correl_1_3 = self.base_frame.correlation('C0', 'C2')
+        C1_C3_columns_data = self.base_frame.take(self.base_frame.row_count, columns=["C0", "C2"]).data
+        numpy_result = numpy.ma.corrcoef(list(C1_C3_columns_data), rowvar=False)
+
+        self.assertAlmostEqual(correl_1_3, float(numpy_result[0][1]))
 
     def test_covar_matrix(self):
         """Verify covariance matrix on all columns"""
-        covar_result = self.get_file("Covariance_400sq_ref.02.txt")
-        covar_ref_frame = self.context.frame.import_csv(covar_result)
+        # create covariance matrix using sparktk
         covar_matrix = self.base_frame.covariance_matrix(self.base_frame.column_names)
-        covar_flat = list(numpy.array(covar_matrix.take(covar_matrix.row_count).data).flat)
-        covar_ref_flat = list(numpy.array(covar_ref_frame.take(covar_ref_frame.row_count).data).flat)
-        for cov_value, res_value in zip(covar_flat, covar_ref_flat):
-            self.assertAlmostEqual(cov_value, res_value, 7)
 
+        # convert to list for ease of comparison
+        covar_flat = list(numpy.array(covar_matrix.take(covar_matrix.row_count).data).flat)
+        numpy_covar_result = list(numpy.cov(list(self.base_frame.take(self.base_frame.row_count).data), rowvar=False))
+        # flatten the numpy result
+        numpy_covar_result = list(numpy.array(numpy_covar_result).flat)
+        
+        # ensure that the data in the covar matrix
+        # matches that which numpy gave us (expected results)
+        for (spark_tk_row, numpy_row) in zip(covar_flat, numpy_covar_result):
+            self.assertAlmostEqual(spark_tk_row, numpy_row)
+    
     def test_correl_matrix(self):
         """Verify correlation matrix on all columns"""
         correl_matrix = self.base_frame.correlation_matrix(self.base_frame.column_names)
-        correl_result = self.get_file("Correlation_400sq_ref.02.txt")
-        correl_ref_frame = self.context.frame.import_csv(correl_result)
+        numpy_correl = list(numpy.ma.corrcoef(list(self.base_frame.take(self.base_frame.row_count).data), rowvar=False))
+        # convert to lists for ease of comparison
         correl_flat = list(numpy.array(correl_matrix.take(correl_matrix.row_count).data).flat)
-        cor_ref_flat = list(numpy.array(correl_ref_frame.take(correl_ref_frame.row_count).data).flat)
-        for correl_value, ref_value in zip(correl_flat, cor_ref_flat):
+        numpy_correl = list(numpy.array(numpy_correl).flat)
+        print "correl matrix sparktk: " + str(correl_flat)
+        print "numpy result: " + str(numpy_correl)
+        # compare the correl matrix values with the expected results
+        for correl_value, ref_value in zip(correl_flat, numpy_correl):
             self.assertAlmostEqual(correl_value, ref_value, 5)
 
 
