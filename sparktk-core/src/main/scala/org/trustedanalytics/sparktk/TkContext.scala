ea778648a7ce918d87e72823684998d9342746fc
Joyesh Mishra <joyesh.mishra@intel.com>
2016-09-12 16:49:20 -0700

    Added Zinc support and Spark Version Mismatch Check

--- sparktk-core/src/main/scala/org/trustedanalytics/sparktk/TkContext.scala
+++ sparktk-core/src/main/scala/org/trustedanalytics/sparktk/TkContext.scala
@@ -1,5 +1,8 @@
 package org.trustedanalytics.sparktk
 
+import java.util.Properties
+import org.slf4j.LoggerFactory
+
 import org.apache.spark.api.java.JavaSparkContext
 import org.trustedanalytics.sparktk.saveload.Loaders
 
@@ -11,6 +14,30 @@ import org.trustedanalytics.sparktk.saveload.Loaders
 class TkContext(jsc: JavaSparkContext) extends Serializable {
 
   val sc = jsc.sc
+
+  private val sparkBuildVersion = {
+    val propertiesFile = this.getClass().getResourceAsStream("/maven.properties")
+    val properties = new Properties()
+    properties.load(propertiesFile)
+    properties.getProperty("dep.spark.version") // From Maven
+  }
+  private val sparkRuntimeVersion = sc.version // From environment
+  private val sparkRuntimeMajorVersion = sparkRuntimeVersion.subSequence(0, sparkRuntimeVersion.lastIndexOf('.'))
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
+  private def checkSparkBuildVersionCompatibility() = {
+    (sparkBuildVersion.contains(sparkRuntimeVersion), sparkBuildVersion.contains(sparkRuntimeMajorVersion)) match {
+      case (true, true) => true
+      case (false, true) =>
+        logger.warn(s"Minor version mismatch: Spark Build Version $sparkBuildVersion " +
+          s"Spark Runtime $sparkRuntimeVersion")
+        true
+      case (_, false) => false
+    }
+  }
+
+  require(checkSparkBuildVersionCompatibility() == true, s"Spark Build Version $sparkBuildVersion " +
+    s"is not compatible with Spark Runtime $sparkRuntimeVersion")
 
   /**
    * Loads a sparktk thing which has been saved at the given path
