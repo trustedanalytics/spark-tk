b874a97f66f6ab8d5772cd313f1e0e7f1b34a2de
Joyesh Mishra <joyesh.mishra@intel.com>
2016-09-14 10:51:10 -0700

    Added Zinc support and Spark Version Mismatch Check (#150)
    * Added Zinc support and Spark Version Mismatch Check
    
    * Fixed per code review
    
    * Minor typo

--- sparktk-core/src/main/scala/org/trustedanalytics/sparktk/TkContext.scala
+++ sparktk-core/src/main/scala/org/trustedanalytics/sparktk/TkContext.scala
@@ -1,5 +1,9 @@
 package org.trustedanalytics.sparktk
 
+import java.util.Properties
+import org.slf4j.LoggerFactory
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion
+
 import org.apache.spark.api.java.JavaSparkContext
 import org.trustedanalytics.sparktk.saveload.Loaders
 
@@ -11,6 +15,29 @@ import org.trustedanalytics.sparktk.saveload.Loaders
 class TkContext(jsc: JavaSparkContext) extends Serializable {
 
   val sc = jsc.sc
+
+  private val sparkBuildVersion = {
+    val propertiesFile = this.getClass.getResourceAsStream("/maven.properties")
+    val properties = new Properties()
+    properties.load(propertiesFile)
+    new DefaultArtifactVersion(properties.getProperty("dep.spark.version")) // From Maven
+  }
+  private val sparkRuntimeVersion = new DefaultArtifactVersion(sc.version) // From environment
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
+  private def checkSparkBuildVersionCompatibility() = {
+    if (sparkBuildVersion.getMajorVersion == sparkRuntimeVersion.getMajorVersion &&
+      sparkBuildVersion.getMinorVersion == sparkRuntimeVersion.getMinorVersion) {
+      if (sparkBuildVersion.getIncrementalVersion != sparkRuntimeVersion.getIncrementalVersion)
+        logger.warn(s"Incremental version mismatch: Spark Build Version $sparkBuildVersion " +
+          s"Spark Runtime $sparkRuntimeVersion")
+      true
+    }
+    else false
+  }
+
+  require(checkSparkBuildVersionCompatibility(), s"Spark Build Version $sparkBuildVersion " +
+    s"is not compatible with Spark Runtime $sparkRuntimeVersion")
 
   /**
    * Loads a sparktk thing which has been saved at the given path
