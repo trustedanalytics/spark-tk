6e2003d72431145a2086e301b1ece6adcba45673
Karthik Vadla <karthik.vadla@intel.com>
2016-09-26 13:55:41 -0700

    add export_to_dcm python version

--- python/sparktk/dicom/ops/export_to_dcm.py
+++ python/sparktk/dicom/ops/export_to_dcm.py
@@ -3,6 +3,51 @@ def export_to_dcm(self, path):
     """
     export_to_dcm creates .dcm image from dicom object with (metadata, imagedata) and saves to given path
 
+    Parameters
+    ----------
+
     :param path: (str) local/hdfs path
 
+
+    Examples
+    --------
+
+        >>> dicom_path = "../datasets/dicom_uncompressed"
+
+        >>> dicom = tc.dicom.import_dcm(dicom_path)
+
+        <skip>
+        >>> dicom.metadata.inspect(truncate=30)
+        [#]  id  metadata
+        =======================================
+        [0]   0  <?xml version="1.0" encodin...
+        [1]   1  <?xml version="1.0" encodin...
+        [2]   2  <?xml version="1.0" encodin...
+        </skip>
+
+        #pixeldata property is sparktk frame
+        >>> pixeldata = dicom.pixeldata.take(1)
+
+        #dispaly
+        <skip>
+        >>> pixeldata
+        TakeResult(data=[[0L, array([[ 0.,  0.,  0., ...,  0.,  0.,  0.],
+        [ 0.,  7.,  5., ...,  5.,  7.,  8.],
+        [ 0.,  7.,  6., ...,  5.,  6.,  7.],
+        ...,
+        [ 0.,  6.,  7., ...,  5.,  5.,  6.],
+        [ 0.,  2.,  5., ...,  5.,  5.,  4.],
+        [ 1.,  1.,  3., ...,  1.,  1.,  0.]])]], schema=[(u'id', <type 'long'>), (u'imagematrix', matrix)])
+        </skip>
+
+        >>> dicom.export_to_dcm("dicom_test_export")
+
     """
+
+    if not isinstance(path, basestring):
+        raise TypeError("path must be a type of string, but found type as " % type(path))
+
+    def f(scala_dicom):
+        scala_dicom.exportToDcm(path)
+
+    self._call_scala(f)
--- sparktk-core/src/main/scala/org/trustedanalytics/sparktk/dicom/internal/ops/ExportToDcm.scala
+++ sparktk-core/src/main/scala/org/trustedanalytics/sparktk/dicom/internal/ops/ExportToDcm.scala
@@ -4,25 +4,23 @@ import java.io.File
 
 import org.apache.commons.io.FileUtils
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.{Path, FileSystem}
+import org.apache.hadoop.fs.{ Path, FileSystem }
 import org.apache.spark.mllib.linalg.DenseMatrix
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
-import org.dcm4che3.data.{VR, Tag}
+import org.dcm4che3.data.{ VR, Tag }
 import org.dcm4che3.io.DicomOutputStream
 import org.dcm4che3.tool.xml2dcm.Xml2Dcm
-import org.trustedanalytics.sparktk.dicom.internal.{DicomState, DicomSummarization, BaseDicom}
-
-import scala.collection.mutable.ArrayBuffer
+import org.trustedanalytics.sparktk.dicom.internal.{ DicomState, DicomSummarization, BaseDicom }
 
 trait ExportToDcmSummarization extends BaseDicom {
   /**
-    * Export .dcm files to given hdfs path.
-    *
-    * Export the dicom images to hadoop path
-    *
-    * @param path The HDFS folder path where the files will be created.
-    */
+   * Export .dcm files to given hdfs path.
+   *
+   * Export the dicom images to hadoop path
+   *
+   * @param path The HDFS folder path where the files will be created.
+   */
   def exportToDcm(path: String) = {
     execute(ExportToDcm(path))
   }
@@ -45,11 +43,13 @@ object ExportToDcm {
 
     zipMetadataPixelData.foreach {
       case (metadata, pixeldata) => {
+
         val oneMetadata = metadata(1).toString
         var onePixeldata: DenseMatrix = pixeldata(1).asInstanceOf[DenseMatrix]
 
-        val tmpFile: File = File.createTempFile(s"dicom-temp", ".xml")
+        val tmpFile: File = File.createTempFile(s"dicom-xml-temp", ".xml")
         FileUtils.writeByteArrayToFile(tmpFile, oneMetadata.getBytes)
+        tmpFile.deleteOnExit()
 
         val dcmAttrs = Xml2Dcm.parseXML(tmpFile.getAbsolutePath)
 
@@ -60,16 +60,16 @@ object ExportToDcm {
         dcmAttrs.setInt(Tag.Columns, VR.US, onePixeldata.numCols)
 
         val exportFile: File = File.createTempFile(s"export", ".dcm")
+        exportFile.deleteOnExit()
+
         val dos: DicomOutputStream = new DicomOutputStream(exportFile)
         dcmAttrs.writeTo(dos)
 
-//        val hdfsPath = "dicom_export/"
-        //System.setProperty("HADOOP_USER_NAME", "kvadla")
         val conf = new Configuration()
         conf.set("fs.defaultFS", "hdfs://10.7.151.97:8020")
         val fs = FileSystem.get(conf)
 
-        val destPath = new Path(path+"/"+ exportFile.getName)
+        val destPath = new Path(path + "/" + exportFile.getName)
         val srcPath = new Path(exportFile.getAbsolutePath)
 
         fs.copyFromLocalFile(srcPath, destPath)
