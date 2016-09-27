c52b1aeab173a34d48dfcbb21ad3124817b22f71
Lewis Coates <lewis.coates@intel.com>
2016-09-27 01:29:01 +0000

    updates tests, currently broken

--- python/sparktk/graph/ops/global_clustering_coefficient.py
+++ python/sparktk/graph/ops/global_clustering_coefficient.py
@@ -16,23 +16,37 @@ def global_clustering_coefficient(self):
     Parameters
     ----------
 
-    :return: (Double) The global clustering coeffcicient of the graph
+    :return: (Double) The global clustering coefficient of the graph
 
     Examples
     --------
 
         >>> vertex_schema = [('id', int)]
-        >>> edge_schema = [('src', int), ('dst', int)]
-
         >>> vertex_rows = [ [1], [2], [3], [4], [5] ]
-        >>> edge_rows = [ [1, 2], [1, 3], [2, 3], [1, 4], [4, 5] ]
         >>> vertex_frame = tc.frame.create(vertex_rows, vertex_schema)
+
+        >>> edge_schema = [('src', int), ('dst', int)]
+        >>> edge_rows = [ [1, 2], [1, 3], [2, 3], [1, 4], [4, 5] ]
         >>> edge_frame = tc.frame.create(edge_rows, edge_schema)
 
         >>> graph = tc.graph.create(vertex_frame, edge_frame)
 
         >>> graph.global_clustering_coefficient()
         0.5
+
+        The clustering coefficient on a graph with no triangles (a tree) is
+        0
+
+        >>> vertex_rows_staar = [ [1], [2], [3], [4]]
+        >>> vertex_frame = tc.frame.create(vertex_rows_star, vertex_schema)
+
+        >>> edge_rows_star = [ [1, 2], [1, 3], [1, 4]]
+        >>> edge_frame = tc.frame.create(edge_rows_star, edge_schema)
+
+        >>> graph_star = tc.graph.create(vertex_frame_star, edge_frame_star)
+
+        >>> graph_star.global_clustering_coefficient()
+        0.0
 
     """
     return self._scala.globalClusteringCoefficient()
