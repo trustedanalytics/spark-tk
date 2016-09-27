29e220719f5ba5f1b4c4ef2ca2105b2c4224ee07
Lewis Coates <lewis.coates@intel.com>
2016-09-27 02:47:02 +0000

    expand paralellization even further

--- regression-tests/automation/run_tests.sh
+++ regression-tests/automation/run_tests.sh
@@ -14,4 +14,4 @@ export SPARKTK_HOME=$MAINDIR/regression-tests/automation/sparktk-core/
 echo "spark tk home"
 echo $SPARKTK_HOME
 
-py.test --boxed -n6 $MAINDIR/regression-tests
+py.test --boxed -n24 $MAINDIR/regression-tests
