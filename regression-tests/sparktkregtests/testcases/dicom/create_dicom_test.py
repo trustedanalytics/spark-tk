bc615d87a1a1e8b3f6c1a42db76788a3c46f2c0a
Haley Bourke <haleyobourke@yahoo.com>
2016-09-28 15:29:44 -0700

    adds dicom images

--- regression-tests/sparktkregtests/testcases/dicom/create_dicom_test.py
+++ regression-tests/sparktkregtests/testcases/dicom/create_dicom_test.py
@@ -23,7 +23,7 @@ class CreateDicomTest(sparktk_test.SparkTKTestCase):
             with open(self.xml_directory + str(filename)) as xmlfile:
                 contents = xmlfile.read()
                 files.append(contents)
-        metadata_pandas = self.dicom.metadata.download()
+        metadata_pandas = self.dicom.metadata.to_pandas()
         for (dcm_file, xml_file) in zip(metadata_pandas["metadata"], files):
             dcm_file = dcm_file.encode("ascii", "ignore")
             bulk_data_index = xml_file.index("<BulkData")
@@ -41,7 +41,7 @@ class CreateDicomTest(sparktk_test.SparkTKTestCase):
             pixel_data = dicom.read_file(self.image_directory + filename).pixel_array
             files.append(pixel_data)
 
-        image_pandas = self.dicom.pixeldata.download()
+        image_pandas = self.dicom.pixeldata.to_pandas()
         for (dcm_image, pixel_image) in zip(image_pandas["imagematrix"], files):
             numpy.testing.assert_equal(pixel_image, dcm_image)
 
@@ -49,13 +49,13 @@ class CreateDicomTest(sparktk_test.SparkTKTestCase):
         dataset = self.get_file("int_str_int.csv")
         with self.assertRaisesRegexp(Exception, "Not a DICOM Stream"):
             dicom = self.context.dicom.import_dcm(dataset)
-            dicom.metadata.take(10)
+            dicom.metadata.count()
 
     def test_import_dicom_mixed_file_types(self):
         dataset = self.get_file("dicom/baddicom/")
         dicom = self.context.dicom.import_dcm(dataset)
         with self.assertRaisesRegexp(Exception, "Not a DICOM Stream"):
-            dicom.metadata.take(10)
+            dicom.metadata.count()
 
 
 if __name__ == "__main__":
