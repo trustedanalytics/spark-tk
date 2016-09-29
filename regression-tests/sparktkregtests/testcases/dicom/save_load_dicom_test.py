2f3574a69dec3746e740a4916a13477f9dcccf6b
Haley Bourke <haleyobourke@yahoo.com>
2016-09-29 14:40:37 -0700

    updates save load dicom test

--- regression-tests/sparktkregtests/testcases/dicom/save_load_dicom_test.py
+++ regression-tests/sparktkregtests/testcases/dicom/save_load_dicom_test.py
@@ -16,19 +16,26 @@ class SaveLoadDicomTest(sparktk_test.SparkTKTestCase):
         super(SaveLoadDicomTest, self).setUp()
         self.dataset = self.get_file("dicom_uncompressed")
         self.dicom = self.context.dicom.import_dcm(self.dataset)
-        self.xml_directory = "../../../datasets/dicom/dicom_uncompressed/xml/"
-        self.image_directory = "../../../datasets/dicom/dicom_uncompressed/imagedata/"
+        self.xml_directory = self.get_local_dataset("dicom/dicom_uncompressed/xml/")
+        self.image_directory = self.get_local_dataset("dicom/dicom_uncompressed/imagedata/")
         # generate a unique name to save the dicom object under
         self.location = "save_load_test" + str(datetime.datetime.now()).replace(":", "-")
 
     def test_basic_save_load_content_test(self):
+        """basic save load content test"""
+        # save the current dicom object under a unique name
         self.dicom.save(self.location)
+        # load the dicom
         load_dicom = self.context.load(self.location)
-        original_metadata = self.dicom.metadata.download()["metadata"]
-        load_metadata = load_dicom.metadata.download()["metadata"]
+        original_metadata = self.dicom.metadata.to_pandas()["metadata"]
+        load_metadata = load_dicom.metadata.to_pandas()["metadata"]
         
+        # compare the loaded dicom object with the dicom object we created
         for (load_row, original_row) in zip(original_metadata, load_metadata):
             original_file = original_row.encode("ascii", "ignore")
+            
+            # extract and remove bulk data element from metadata since we don't care about it
+            # bulk data records the file's location, so it may differ
             loaded_file = load_row.encode("ascii", "ignore")
             bulk_data_index = original_file.index("<BulkData")
             load_bulk_data = loaded_file[bulk_data_index:bulk_data_index + loaded_file[bulk_data_index:].index(">") + 1]
@@ -38,25 +45,31 @@ class SaveLoadDicomTest(sparktk_test.SparkTKTestCase):
   
             self.assertEqual(loaded_file, original_file)
  
-        original_image = self.dicom.pixeldata.download()
-        loaded_image = load_dicom.pixeldata.download()
+        # now we check that the pixel data matches
+        original_image = self.dicom.pixeldata.to_pandas()
+        loaded_image = load_dicom.pixeldata.to_pandas()
         for (dcm_image, pixel_image) in zip(original_image["imagematrix"], loaded_image["imagematrix"]):
             numpy.testing.assert_equal(pixel_image, dcm_image)
 
     def test_save_invalid_long_unicode_name(self):
-        metadata_unicode = self.dicom.metadata.download()["metadata"][0]
+        """save under a long unicode name, should fail"""
+        # we will pass the dicom metadata itself as the name
+        metadata_unicode = self.dicom.metadata.to_pandas()["metadata"][0]
         with self.assertRaisesRegexp(Exception, "IllegalArgumentException"):
             self.dicom.save(metadata_unicode)
 
     def test_load_does_not_exist(self):
+        """test load dicom does not exist"""
         with self.assertRaisesRegexp(Exception, "Input path does not exist"):
             self.context.load("does_not_exist")
 
     def test_save_invalid_path_type(self):
+        """test save dicom invalid path type"""
         with self.assertRaisesRegexp(Exception, "does not exist"):
             self.dicom.save(1)
 
     def test_save_name_already_exists(self):
+        """test save dicom duplicate name"""
         with self.assertRaisesRegexp(Exception, "already exists"):
             self.dicom.save("duplicate_name")
             self.dicom.save("duplicate_name")
