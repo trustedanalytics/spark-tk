929c197692d418d9d85abf777a36edde61922603
rodorad <rene.o.dorado@intel.com>
2016-10-03 10:41:11 -0700

    add license execptions and add license headers to install scripts.

--- sparktk-core/install.sh
+++ sparktk-core/install.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#
+#  Copyright (c) 2016 Intel Corporation 
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
 ##Install the sparktk-core python package and very the users system is ready sparktk.
 ##Warnings will be displayed for missing pacakages or ENV variables.
 ## - see if SPARK_HOME is set
