99280694adc34b02883febfc71c610638808710c
Lewis Coates <lewis.coates@intel.com>
2016-10-11 10:27:28 -0700

    fixes correlation matrix and removes some NaN scrubbing that is unnecessary (#211)

--- sparktk-core/src/test/scala/org/trustedanalytics/sparktk/frame/internal/ops/statistics/correlation/CorrelationMatrixTest.scala
+++ sparktk-core/src/test/scala/org/trustedanalytics/sparktk/frame/internal/ops/statistics/correlation/CorrelationMatrixTest.scala
@@ -46,5 +46,26 @@ class CorrelationMatrixTest extends TestingSparkContextWordSpec with Matchers {
       result(1) shouldBe Row(0.8451542547285167, 1.0, 0.0)
       result(2) shouldBe Row(0.2988071523335984, 0.0, 1.0)
     }
+    "return the correct Nan Values" in {
+      val inputArrayNan: Array[Array[Double]] = Array(Array(0, 1.0, 4.0, 0.0, -1.0), Array(1, 2.0, 3.0, 0.0, -1.0), Array(2, 3.0, 2.0, 1.0, -1.0), Array(3, 4.0, 1.0, 2.0, -1.0), Array(4, 5.0, 0.0, 2.0, -1.0))
+      val arrGenericRow: Array[Row] = inputArrayNan.map(row => {
+        val temp: Array[Any] = row.map(x => x)
+        new GenericRow(temp)
+      })
+
+      val rdd = sparkContext.parallelize(arrGenericRow)
+      val columnsList = List("col_0", "col_1", "col_2", "col_3", "col_4")
+      val inputDataColumnNamesAndTypes: Vector[Column] = columnsList.map({ name => Column(name, DataTypes.float64) }).toVector
+      val schema = FrameSchema(inputDataColumnNamesAndTypes)
+      val frameRdd = new FrameRdd(schema, rdd)
+      val result = CorrelationFunctions.correlationMatrix(frameRdd, columnsList).collect()
+      result.size shouldBe 5
+
+      result(0) shouldBe Row(1.0, 0.9999999999999998, -0.9999999999999998, 0.9486832980505138, Double.NaN)
+      result(1) shouldBe Row(0.9999999999999998, 1.0, -0.9999999999999998, 0.9486832980505138, Double.NaN)
+      result(2) shouldBe Row(-0.9999999999999998, -0.9999999999999998, 1.0, -0.9486832980505138, Double.NaN)
+      result(3) shouldBe Row(0.9486832980505138, 0.9486832980505138, -0.9486832980505138, 1.0, Double.NaN)
+      result(4) shouldBe Row(Double.NaN, Double.NaN, Double.NaN, Double.NaN, 1.0)
+    }
   }
 }
