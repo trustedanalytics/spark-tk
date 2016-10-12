190096bb8fff9eefb40955fd2b3b3661d59a46ed
Karthik Vadla <karthik.vadla@intel.com>
2016-10-12 11:42:35 -0700

    Dicom all - All bug fixes, added features. (#226)
    * add extractkeywords feature, end to end flow completed
    
    * add ExtractKeywords and ExtractTags features. Add skip to dicom.py to avoid integration test delay
    
    * code modifications
    
    * Handle dicom mutability, check is_scala or is_python and then run udfs - performance concern
    to avoid unnecessary conversions.
    
    * merge fixes from DPNG-10839-features-filter
    
    * Crucible feedback changes
    
    * add ExtractKeywords, ExtractTags unit tests in scala, add crucible changes
    
    * comments about mutability
    
    * add FilterByKeywords, FilterByTags, DropRowsByKeywords, DropRowsByTags scala functionalities and unit test cases
    
    * add code formatting
    
    * add filter_by_keywords, filter_by_tags, drop_rows_by_keywords, drop_rows_by_tags python implementaions.
    
    * export to dcm initial test
    
    * add ExportToDcm scala functionality and test case
    
    * add export_to_dcm python version
    
    * remove duplicate logic, add content for doc api
    
    * add comments in doc api
    
    * add doc api changes
    
    * add changes to export_to_dcm scala, hdfs conf setup
    
    * add formatting changes
    
    * add formating changes
    
    * doc changes
    
    * comments in test
    
    * add using StringUtils.join to create path
    
    * checkpoint
    
    * add fix to pixeldata display issue, now matrix is stored as numpy.ndarray in rdd. And conversions between _python to _scala and viceversa handled.
    
    * remove dicom compressed files
    
    * add isTransposed=True while converting matrix from python to scala
    
    * Initial commit
    
    * Fix
    
    * Add doc api formatting, add new dicom images, bug fix for list index out of bound- Stable code
    
    * Add crucible feedback changes
    
    * checkpoint
    
    * isTranspose removed
    
    * checkpoint, add some crucible changes
    
    * transpose fix check
    
    * added transpose to exportDCM
    
    * add comments for colum/row major storage

--- sparktk-core/src/test/scala/org/trustedanalytics/sparktk/dicom/internal/ops/ExportToDcmTest.scala
+++ sparktk-core/src/test/scala/org/trustedanalytics/sparktk/dicom/internal/ops/ExportToDcmTest.scala
@@ -15,33 +15,24 @@
  */
 package org.trustedanalytics.sparktk.dicom.internal.ops
 
+import java.io.File
+
 import org.scalatest.Matchers
-import org.trustedanalytics.sparktk.testutils.TestingSparkContextWordSpec
 import org.trustedanalytics.sparktk.dicom.internal.constructors.Import.importDcm
+import org.trustedanalytics.sparktk.testutils.TestingSparkContextWordSpec
 
-class ExtractTagsTest extends TestingSparkContextWordSpec with Matchers {
+class ExportToDcmTest extends TestingSparkContextWordSpec with Matchers {
 
-  "Extract Tags" should {
-    "extract value for each tag and add column for each tag to assign value. For missing tag, value is null" in {
+  "ExportToDcm" should {
+    "create new .dcm from metadata and pixeldata and export to specified hdfs/local path" in {
       val dicom = importDcm(sparkContext, "../integration-tests/datasets/dicom_uncompressed")
 
-      val columnCount = dicom.metadata.schema.columnNames.length
+      val destPath = "../integration-tests/tests/sandbox/dicom_export"
+      dicom.exportToDcm(destPath)
 
-      columnCount shouldBe 2
+      val export_status = new File(destPath).exists()
+      assert(export_status.equals(true))
 
-      val tags = Seq("00080018", "00080070", "00080020")
-
-      dicom.extractTags(tags)
-
-      val newColumnCount = dicom.metadata.schema.columnNames.length
-
-      newColumnCount shouldBe 5
-
-      val expectedColNames = Seq("id", "metadata", "00080018", "00080070", "00080020")
-
-      val actualColNames = dicom.metadata.schema.columnNames
-
-      actualColNames should contain theSameElementsAs (expectedColNames)
     }
   }
 }
