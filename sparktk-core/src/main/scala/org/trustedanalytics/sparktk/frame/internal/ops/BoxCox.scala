79012eeab7c5a408d0288ed709f5c1f1411b4f1c
Anahita Bhiwandiwalla <anahita.bhiwandiwalla@intel.com>
2016-10-12 19:43:31 -0700

    Boxcox and reverse box cox fix for optional column name

--- sparktk-core/src/main/scala/org/trustedanalytics/sparktk/frame/internal/ops/BoxCox.scala
+++ sparktk-core/src/main/scala/org/trustedanalytics/sparktk/frame/internal/ops/BoxCox.scala
@@ -23,7 +23,21 @@ import org.trustedanalytics.sparktk.frame.internal.{ BaseFrame, FrameState, Fram
 import org.trustedanalytics.sparktk.frame.internal.rdd.FrameRdd
 
 trait BoxCoxTransform extends BaseFrame {
-
+  /**
+   * Computes the box-cox transformation for a column in the frame
+   * @param columnName Name of the column to perform Box-cox transform on
+   * @param lambdaValue Lambda Power Parameter
+   * @param boxCoxColumnName Optional parameter specifying the name of column storing box-cox computed value
+   * Calculate the box-cox transformation for each row in a frame using the given lambda value or default 0.0.
+   *
+   *  The box-cox transformation is computed by the following formula, where yt is a single entry value(row):
+   *
+   *  wt = log(yt); if lambda=0,
+   *  wt = (yt^lambda -1)/lambda ; else
+   *
+   *  where log is the natural log.
+   *
+   */
   def boxCox(columnName: String, lambdaValue: Double = 0d, boxCoxColumnName: Option[String] = None): Unit = {
     execute(BoxCox(columnName, lambdaValue, boxCoxColumnName))
   }
@@ -35,7 +49,7 @@ case class BoxCox(columnName: String, lambdaValue: Double, boxCoxColumnName: Opt
 
   override def work(state: FrameState): FrameState = {
     // run the operation
-    val boxCoxRdd = BoxCox.boxCox(state, columnName, lambdaValue, boxCoxColumnName)
+    val boxCoxRdd = BoxCox.boxCox(state, columnName, lambdaValue)
 
     // save results
     val updatedSchema = state.schema.addColumn(boxCoxColumnName.getOrElse(columnName + "_lambda_" + lambdaValue.toString), DataTypes.float64)
@@ -46,10 +60,13 @@ case class BoxCox(columnName: String, lambdaValue: Double, boxCoxColumnName: Opt
 
 object BoxCox extends Serializable {
   /**
-   * Computes the boxcox transform for each row of the frame
-   *
+   * Computes the box-cox transformation for a column in the frame
+   * @param frameRdd Frame storing the data
+   * @param columnName Name of the column to perform Box-cox transform on
+   * @param lambdaValue Lambda power parameter
+   * @return Return a RDD[Row] with a box-cox transform added to existing data
    */
-  def boxCox(frameRdd: FrameRdd, columnName: String, lambdaValue: Double, boxCoxColumnName: Option[String] = None): RDD[Row] = {
+  def boxCox(frameRdd: FrameRdd, columnName: String, lambdaValue: Double): RDD[Row] = {
     frameRdd.mapRows(row => {
       val y = row.doubleValue(columnName)
       val boxCox = computeBoxCoxTransformation(y, lambdaValue)
@@ -57,6 +74,12 @@ object BoxCox extends Serializable {
     })
   }
 
+  /**
+   * Compute the box-cox transformation
+   * @param y The value whose box-cox is to be computed
+   * @param lambdaValue Lambda power parameter
+   * @return Box-cox transformed value
+   */
   def computeBoxCoxTransformation(y: Double, lambdaValue: Double): Double = {
     val boxCox: Double = if (lambdaValue == 0d) math.log(y) else (math.pow(y, lambdaValue) - 1) / lambdaValue
     boxCox
