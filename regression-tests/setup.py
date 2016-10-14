f868a5dd21cc8edf8cc550b279ef968ce8f2c6ea
rodorad <rene.o.dorado@intel.com>
2016-10-14 10:19:59 -0700

    create installable sparktk package (#176)
    * create installable sparktk package
    
    * remove parallel flag
    
    * add version and posttag to spatktk zip package
    
    * echo module name
    
    * force pip update
    
    * create python file for regression test
    
    * rebase from mater, run spark version check in local mode
    
    * remove unnecessary xml config
    
    * add license execptions and add license headers to install scripts.
    
    * install package
    
    * changes to install sparktkpackage
    
    * path change
    
    * lower parallel builds to 1 to see if it passes
    
    * copy graph frames to module path, look for correct python and pip versions
    
    * add lic header
    
    * increase test runners
    
    * increase test runners

--- regression-tests/setup.py
+++ regression-tests/setup.py
@@ -24,12 +24,11 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 POST="DEV"
 BUILD="0"
-
 VERSION="0.7"
 
 setup(
     # Application name:
-    name="sparktk",
+    name="sparktk-regression-tests",
 
     version="{0}-{1}{2}".format(VERSION, POST, BUILD),
 
@@ -38,7 +37,7 @@ setup(
 
 
     # Packages
-    packages=["sparktk"],
+    packages=["automation", "datasets", "exampletests", "generatedata", "sparktkregtests"],
 
     # Include additional files into the package
     include_package_data=True,
@@ -51,9 +50,6 @@ setup(
 
     description="spark-tk is a library which enhances the Spark experience by providing a rich, easy-to-use API for Python and Scala.",
 
-    long_description=open("README.rst").read(),
-
     # Dependent packages (distributions)
     install_requires=reqs,
-
-)
+    )
