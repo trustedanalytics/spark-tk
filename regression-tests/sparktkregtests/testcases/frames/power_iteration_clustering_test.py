dca2fe0b3105ed32d13742ba0501e204b13b4138
Lewis Coates <lewis.coates@intel.com>
2016-11-15 15:39:47 -0800

    fix

--- regression-tests/sparktkregtests/testcases/frames/power_iteration_clustering_test.py
+++ regression-tests/sparktkregtests/testcases/frames/power_iteration_clustering_test.py
@@ -56,12 +56,10 @@ class PowerIterationTest(sparktk_test.SparkTKTestCase):
         #check values assigned to each cluster
         actual_assignment = result.frame.to_pandas(
             result.frame.count()).groupby("cluster")
-        print result.frame.to_pandas(result.frame.count())
-        print frame.to_pandas(frame.count())
         grouped_assignment = [list(val["id"]) for index, val in actual_assignment]
 
         expected_assignment = [[4, 5, 6], [1, 2, 3], [0]]
-        self.assertEqual(sorted(grouped_assignment), sorted(expected_assignment))
+        self.assertEqual(sorted(map(sorted, grouped_assignment)), sorted(map(sorted, expected_assignment)))
 
     def test_circles_default(self):
         """ Test pic on similarity matrix for two concentric cicles """
