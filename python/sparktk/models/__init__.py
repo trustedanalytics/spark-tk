8272b0d2f07f4f890dd8310e96cb978cdd79f6d6
Briton Barker <briton.barker@intel.com>
2016-11-18 18:12:36 -0800

    fix lazyloader to check __init__ directly for methods, add tmp _soup/ for cross_validation work

--- python/sparktk/models/__init__.py
+++ python/sparktk/models/__init__.py
@@ -14,4 +14,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
+from sparktk import TkContext
+from _soup.cross_validate import cross_validate
+
 
