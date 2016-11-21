bd64301c2c10694dc5a17cddd11aa847ead047e0
wafaa Taie <wafaa.s.taie@intel.com>
2016-11-21 11:05:50 -0800

    Add test for SSSP against larger graph size

--- sparktk-core/src/main/scala/org/apache/spark/graphx/lib/SingleSourceShortestPath.scala
+++ sparktk-core/src/main/scala/org/apache/spark/graphx/lib/SingleSourceShortestPath.scala
@@ -26,82 +26,14 @@ import scala.reflect.ClassTag
  * vertex attribute is a map containing the shortest-path distance to each reachable landmark.
  */
 object SingleSourceShortestPath {
- /* /** Stores a map from the vertex id of a landmark to the distance to that landmark. */
-  type SPMap = Map[VertexId, Double]
 
-  private def makeMap(x: (VertexId, Double)*) = Map(x: _*)
-
-  private def incrementMap(spmap: SPMap): SPMap = spmap.map { case (v, d) => v -> (d + 1) }
-
-  private def addMaps(spmap1: SPMap, spmap2: SPMap): SPMap =
-    (spmap1.keySet ++ spmap2.keySet).map {
-      k => k -> math.min(spmap1.getOrElse(k, Double.MaxValue), spmap2.getOrElse(k, Double.MaxValue))
-    }.toMap
-
-  /**
-   * Computes shortest paths to the given set of landmark vertices.
-   *
-   * @tparam ED the edge attribute type (not used in the computation)
-    * @param graph the graph for which to compute the shortest paths
-   * @param landmarks the list of landmark vertex ids. Shortest paths will be computed to each
-   * landmark.
-    * @return a graph where each vertex attribute is a map containing the shortest-path distance to
-   * each reachable landmark vertex.
-   */
-  def run[VD, ED: ClassTag](graph: Graph[VD, ED], landmarks: Seq[VertexId],weights:Boolean): Graph[SPMap, Double] = {
-    /*val spGraph = graph.mapVertices { (vid, attr) =>
-      if (landmarks.contains(vid)) makeMap(vid -> 0) else makeMap()
-    }*/
-
-   /* val spGraph = graph.mapTriplets(tri =>{
-      if (landmarks.contains(tri.dstId)) {
-        if (weights)
-          makeMap(tri.dstId -> 0) else makeMap(tri.dstId -> 0)
-      }else{
-        makeMap()
-    }
-    })*/
-
-    val spGraph = graph.mapVertices { (vid, attr) =>
-      graph.mapTriplets(tri => {
-        if (landmarks.contains(vid)) {
-          if (weights)
-            if (tri.dstId == vid)
-              val vid = graph.vertices.
-              makeMap(tri -> 0)
-            else makeMap(vid -> 0)
-        } else {
-          makeMap()
-        }
-      })
-    }
-
-
-
-    val initialMessage = makeMap()
-
-    def vertexProgram(id: VertexId, attr: SPMap, msg: SPMap): SPMap = {
-      addMaps(attr, msg)
-    }
-
-    def sendMessage(edge: EdgeTriplet[SPMap, Double]): Iterator[(VertexId, SPMap)] = {
-      val newAttr = incrementMap(edge.dstAttr)
-      if (edge.srcAttr != addMaps(newAttr, edge.srcAttr)) Iterator((edge.srcId, newAttr))
-      else Iterator.empty
-    }
-
-    Pregel(spGraph, initialMessage)(vertexProgram, sendMessage, addMaps)
-  }*/
-
-
- // initialize all vertices except the root to have distance infinity
   type SpTuple= (Double, List[VertexId])
 
-  private def updateSp(edge: EdgeTriplet[SpTuple,Double],edgeWeightAttribute: Option[String] = None):(SpTuple,Double) ={
+  private def updateSp(edge: EdgeTriplet[SpTuple,Double],useEdgeWeight: Boolean = false):(SpTuple,Double) ={
     val dist = edge.srcAttr._1
     val path = edge.srcAttr._2
     val newPath = path :+ edge.dstId
-    val weight = if(edgeWeightAttribute.isDefined) edge.attr else 1.0
+    val weight = if(useEdgeWeight) edge.attr else 1.0
     val newDist = dist + weight
     ((newDist,newPath),weight)
   }
@@ -109,7 +41,7 @@ object SingleSourceShortestPath {
 
   def run[VD, ED: ClassTag](graph: Graph[VD,ED],
                             srcVertexId: VertexId,
-                            edgeWeightAttribute: Option[String] = None,
+                            edgeWeightAttribute: Boolean = false,
                             landmarks: Option[Seq[VertexId]] = None,
                             maxPathLength: Option[Double] = None): Graph[SpTuple, Double] = {
 
@@ -118,8 +50,8 @@ object SingleSourceShortestPath {
       val distAtSrc = edge.srcAttr._1
       val distAtDest = edge.dstAttr._1
       //TODO: check how to separate max path length from weight and how to address them in each case (these conditions need debug)
-      if ((landmarks.isDefined && landmarks.get.contains(edge.dstId)) ||
-        (maxPathLength.isDefined && distAtDest >= maxPathLength.get + 1) ||
+      if ((landmarks.isDefined && !landmarks.get.contains(edge.dstId)) ||
+        (maxPathLength.isDefined && distAtSrc == maxPathLength.get) ||
         (distAtSrc > distAtDest - weight)) {
         Iterator.empty
       }else{
--- sparktk-core/src/test/scala/org/apache/spark/graphx/lib/SingleSourceShortestPath$Test.scala
+++ sparktk-core/src/test/scala/org/apache/spark/graphx/lib/SingleSourceShortestPath$Test.scala
@@ -11,14 +11,15 @@ class SingleSourceShortestPath$Test extends TestingSparkContextWordSpec with Mat
 
   "Shortest paths" should{
 
-    "find SPs" in {
 
+    "find SPs" in {
+      var results:Graph[(Double, List[VertexId]),Double] = null
       // create vertices RDD with ID and Name
-      val vertices=Array((1L, ("SFO")),(2L, ("ORD")),(3L,("DFW")))
+      val vertices=Array((1L, ("SFO")),(2L, ("ORD")),(3L,("DFW")),(4L,("PDX")),(5L,("LAX")),(6L,("LLL")),(7L,("IAF")))
       val vRDD= sparkContext.parallelize(vertices)
 
       // create routes RDD with srcid, destid, distance
-      val edges = Array(Edge(1L,2L,1800.0),Edge(2L,3L,800.0),Edge(3L,1L,1400.0))
+      val edges = Array(Edge(1L,2L,1800.0),Edge(2L,3L,800.0),Edge(3L,1L,1400.0),Edge(2L,4L,900.0),Edge(1L,7L,1100.0),Edge(3L,5L,700.0), Edge(4L,6L,600.0),Edge(1L,6L,500.0))
       val eRDD= sparkContext.parallelize(edges)
 
       // define the graph
@@ -36,7 +37,7 @@ class SingleSourceShortestPath$Test extends TestingSparkContextWordSpec with Mat
       // Edge(2,3,800)
       // Edge(3,1,1400)
 
-      val results = SingleSourceShortestPath.run(graph,1,None,None,None)
+      results = SingleSourceShortestPath.run(graph,3,false,Some(Seq(3,1,5,6)),Some(4.0))
       println(results.vertices.collect.mkString("\n"))
 
       }
