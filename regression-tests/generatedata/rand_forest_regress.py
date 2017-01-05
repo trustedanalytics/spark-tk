1290208dc9e8fba19a5d5189a860f8a026324bf9
Lewis Coates <lewis.coates@intel.com>
2017-01-05 14:48:58 -0800

    Adds tests for random forest in the case of a very deep (> 32 layers) tree.

---
+++ regression-tests/generatedata/rand_forest_regress.py
@@ -0,0 +1,28 @@
+# vim: set encoding=utf-8
+
+#  Copyright (c) 2016 Intel Corporation 
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+import random
+import operator as o
+
+def rand_forest_class(features, ptcnt):
+    """Generates data random forest dataset with 2 features"""
+    for i in xrange(ptcnt):
+       r = [random.randint(-100,100) for i in xrange(features)]
+       print ",".join(map(str, r))
+    
+if __name__ == "__main__":
+    rand_forest_class(1001, 10**4)
