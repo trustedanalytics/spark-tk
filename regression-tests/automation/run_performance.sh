82dcb865eee9689a4efd8f0b49011c6b58149c65
Lewis Coates <lewis.coates@intel.com>
2017-01-11 17:42:27 -0800

    allowing temcity messages to pass through

--- regression-tests/automation/run_performance.sh
+++ regression-tests/automation/run_performance.sh
@@ -35,4 +35,4 @@ export SPARKTK_HOME=$sparktkpackage/
 echo "spark tk home"
 echo $SPARKTK_HOME
 
-py.test -v $MAINDIR/regression-tests/sparktkregtests/testcases/performance
+py.test -s -v $MAINDIR/regression-tests/sparktkregtests/testcases/performance
